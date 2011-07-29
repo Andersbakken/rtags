0da728a7b51c94518b2357e55025e1df11b7d4fb
Jan Erik Hanssen <jhanssen@gmail.com>
2011-07-29 01:45:34 -0700

    Reorganize the header file for better readability

--- PreCompile.h
+++ PreCompile.h
@@ -12,11 +12,13 @@ class PreCompile
 public:
     ~PreCompile();
 
-    static void setPath(const QString& path);
     static PreCompile* get(const QList<QByteArray>& args);
+
     static void clearAll();
     static void clear(const QByteArray& header);
     static void clear(const QList<QByteArray>& args);
+
+    static void setPath(const QString& path);
 
     void add(const QList<QByteArray>& direct, const QList<QByteArray>& all);
     QString filename() const;
