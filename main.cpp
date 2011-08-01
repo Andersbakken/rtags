a854d8185c50c5026a260b6b99d4684593c851c0
Anders Bakken <agbakken@gmail.com>
2011-07-31 21:06:25 -0700

    Better logging

--- main.cpp
+++ main.cpp
@@ -48,7 +48,11 @@ void syslogMsgHandler(QtMsgType t, const char* str)
     QFile file("/tmp/rtags.log");
     file.open(QIODevice::WriteOnly|QIODevice::Append);
     char buf[1024];
-    const int s = snprintf(buf, 1023, "%s (%s)\n", str, names[t]);
+    const int s = snprintf(buf, 1023, "%s (%s): %s (%s)\n",
+                           qPrintable(QDateTime::currentDateTime().toString()),
+                           qPrintable(QThread::currentThread()->objectName()),
+                           str,
+                           names[t]);
     file.write(buf, s);
     syslog(priority, "%s (%s)\n", str, names[t]);
 }
