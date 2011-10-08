0ddad5200d4921cb8cdbfebccf8964444cc1066c
Anders Bakken <agbakken@gmail.com>
2011-10-08 12:41:01 -0700

    Add more kinds

--- rbuild/Utils.cpp
+++ rbuild/Utils.cpp
@@ -2,6 +2,79 @@
 #include <clang-c/Index.h>
 #include "Path.h"
 
+#ifdef OLDASS_CLANG
+enum { CXCursor_CXXAccessSpecifier,
+       CXCursor_IntegerLiteral,
+       CXCursor_FloatingLiteral,
+       CXCursor_ImaginaryLiteral,
+       CXCursor_StringLiteral,
+       CXCursor_CharacterLiteral,
+       CXCursor_ParenExpr,
+       CXCursor_UnaryOperator,
+       CXCursor_ArraySubscriptExpr,
+       CXCursor_BinaryOperator,
+       CXCursor_CompoundAssignOperator,
+       CXCursor_ConditionalOperator,
+       CXCursor_CStyleCastExpr,
+       CXCursor_CompoundLiteralExpr,
+       CXCursor_InitListExpr,
+       CXCursor_AddrLabelExpr,
+       CXCursor_StmtExpr,
+       CXCursor_GenericSelectionExpr,
+       CXCursor_GNUNullExpr,
+       CXCursor_CXXStaticCastExpr,
+       CXCursor_CXXDynamicCastExpr,
+       CXCursor_CXXReinterpretCastExpr,
+       CXCursor_CXXConstCastExpr,
+       CXCursor_CXXFunctionalCastExpr,
+       CXCursor_CXXTypeidExpr,
+       CXCursor_CXXBoolLiteralExpr,
+       CXCursor_CXXNullPtrLiteralExpr,
+       CXCursor_CXXThisExpr,
+       CXCursor_CXXThrowExpr,
+       CXCursor_CXXNewExpr,
+       CXCursor_CXXDeleteExpr,
+       CXCursor_UnaryExpr,
+       CXCursor_ObjCStringLiteral,
+       CXCursor_ObjCEncodeExpr,
+       CXCursor_ObjCSelectorExpr,
+       CXCursor_ObjCProtocolExpr,
+       CXCursor_ObjCBridgedCastExpr,
+       CXCursor_PackExpansionExpr,
+       CXCursor_SizeOfPackExpr,
+       CXCursor_LastExpr,
+       CXCursor_CompoundStmt,
+       CXCursor_CaseStmt,
+       CXCursor_DefaultStmt,
+       CXCursor_IfStmt,
+       CXCursor_SwitchStmt,
+       CXCursor_WhileStmt,
+       CXCursor_DoStmt,
+       CXCursor_ForStmt,
+       CXCursor_GotoStmt,
+       CXCursor_IndirectGotoStmt,
+       CXCursor_ContinueStmt,
+       CXCursor_BreakStmt,
+       CXCursor_ReturnStmt,
+       CXCursor_AsmStmt,
+       CXCursor_ObjCAtTryStmt,
+       CXCursor_ObjCAtCatchStmt,
+       CXCursor_ObjCAtFinallyStmt,
+       CXCursor_ObjCAtThrowStmt,
+       CXCursor_ObjCAtSynchronizedStmt,
+       CXCursor_ObjCAutoreleasePoolStmt,
+       CXCursor_ObjCForCollectionStmt,
+       CXCursor_CXXCatchStmt,
+       CXCursor_CXXTryStmt,
+       CXCursor_CXXForRangeStmt,
+       CXCursor_SEHTryStmt,
+       CXCursor_SEHExceptStmt,
+       CXCursor_SEHFinallyStmt,
+       CXCursor_NullStmt,
+       CXCursor_DeclStmt,
+       CXCursor_LastStmt
+};
+#endif
 const char *kindToString(CXCursorKind kind)
 {
     switch (kind) {
@@ -91,7 +164,74 @@ const char *kindToString(CXCursorKind kind)
     case CXCursor_CXXOverrideAttr: return "CXCursor_CXXOverrideAttr";
         // case CXCursor_FirstPreprocessing: return "CXCursor_FirstPreprocessing";
         // case CXCursor_LastPreprocessing: return "CXCursor_LastPreprocessing";
-    // case CXCursor_CXXAccessSpecifier: return "CXCursor_CXXAccessSpecifier";
+    case CXCursor_CXXAccessSpecifier: return "CXCursor_CXXAccessSpecifier";
+    case CXCursor_IntegerLiteral: return "CXCursor_IntegerLiteral";
+    case CXCursor_FloatingLiteral: return "CXCursor_FloatingLiteral";
+    case CXCursor_ImaginaryLiteral: return "CXCursor_ImaginaryLiteral";
+    case CXCursor_StringLiteral: return "CXCursor_StringLiteral";
+    case CXCursor_CharacterLiteral: return "CXCursor_CharacterLiteral";
+    case CXCursor_ParenExpr: return "CXCursor_ParenExpr";
+    case CXCursor_UnaryOperator: return "CXCursor_UnaryOperator";
+    case CXCursor_ArraySubscriptExpr: return "CXCursor_ArraySubscriptExpr";
+    case CXCursor_BinaryOperator: return "CXCursor_BinaryOperator";
+    case CXCursor_CompoundAssignOperator: return "CXCursor_CompoundAssignOperator";
+    case CXCursor_ConditionalOperator: return "CXCursor_ConditionalOperator";
+    case CXCursor_CStyleCastExpr: return "CXCursor_CStyleCastExpr";
+    case CXCursor_CompoundLiteralExpr: return "CXCursor_CompoundLiteralExpr";
+    case CXCursor_InitListExpr: return "CXCursor_InitListExpr";
+    case CXCursor_AddrLabelExpr: return "CXCursor_AddrLabelExpr";
+    case CXCursor_StmtExpr: return "CXCursor_StmtExpr";
+    case CXCursor_GenericSelectionExpr: return "CXCursor_GenericSelectionExpr";
+    case CXCursor_GNUNullExpr: return "CXCursor_GNUNullExpr";
+    case CXCursor_CXXStaticCastExpr: return "CXCursor_CXXStaticCastExpr";
+    case CXCursor_CXXDynamicCastExpr: return "CXCursor_CXXDynamicCastExpr";
+    case CXCursor_CXXReinterpretCastExpr: return "CXCursor_CXXReinterpretCastExpr";
+    case CXCursor_CXXConstCastExpr: return "CXCursor_CXXConstCastExpr";
+    case CXCursor_CXXFunctionalCastExpr: return "CXCursor_CXXFunctionalCastExpr";
+    case CXCursor_CXXTypeidExpr: return "CXCursor_CXXTypeidExpr";
+    case CXCursor_CXXBoolLiteralExpr: return "CXCursor_CXXBoolLiteralExpr";
+    case CXCursor_CXXNullPtrLiteralExpr: return "CXCursor_CXXNullPtrLiteralExpr";
+    case CXCursor_CXXThisExpr: return "CXCursor_CXXThisExpr";
+    case CXCursor_CXXThrowExpr: return "CXCursor_CXXThrowExpr";
+    case CXCursor_CXXNewExpr: return "CXCursor_CXXNewExpr";
+    case CXCursor_CXXDeleteExpr: return "CXCursor_CXXDeleteExpr";
+    case CXCursor_UnaryExpr: return "CXCursor_UnaryExpr";
+    case CXCursor_ObjCStringLiteral: return "CXCursor_ObjCStringLiteral";
+    case CXCursor_ObjCEncodeExpr: return "CXCursor_ObjCEncodeExpr";
+    case CXCursor_ObjCSelectorExpr: return "CXCursor_ObjCSelectorExpr";
+    case CXCursor_ObjCProtocolExpr: return "CXCursor_ObjCProtocolExpr";
+    case CXCursor_ObjCBridgedCastExpr: return "CXCursor_ObjCBridgedCastExpr";
+    case CXCursor_PackExpansionExpr: return "CXCursor_PackExpansionExpr";
+    case CXCursor_SizeOfPackExpr: return "CXCursor_SizeOfPackExpr";
+    case CXCursor_CompoundStmt: return "CXCursor_CompoundStmt";
+    case CXCursor_CaseStmt: return "CXCursor_CaseStmt";
+    case CXCursor_DefaultStmt: return "CXCursor_DefaultStmt";
+    case CXCursor_IfStmt: return "CXCursor_IfStmt";
+    case CXCursor_SwitchStmt: return "CXCursor_SwitchStmt";
+    case CXCursor_WhileStmt: return "CXCursor_WhileStmt";
+    case CXCursor_DoStmt: return "CXCursor_DoStmt";
+    case CXCursor_ForStmt: return "CXCursor_ForStmt";
+    case CXCursor_GotoStmt: return "CXCursor_GotoStmt";
+    case CXCursor_IndirectGotoStmt: return "CXCursor_IndirectGotoStmt";
+    case CXCursor_ContinueStmt: return "CXCursor_ContinueStmt";
+    case CXCursor_BreakStmt: return "CXCursor_BreakStmt";
+    case CXCursor_ReturnStmt: return "CXCursor_ReturnStmt";
+    case CXCursor_AsmStmt: return "CXCursor_AsmStmt";
+    case CXCursor_ObjCAtTryStmt: return "CXCursor_ObjCAtTryStmt";
+    case CXCursor_ObjCAtCatchStmt: return "CXCursor_ObjCAtCatchStmt";
+    case CXCursor_ObjCAtFinallyStmt: return "CXCursor_ObjCAtFinallyStmt";
+    case CXCursor_ObjCAtThrowStmt: return "CXCursor_ObjCAtThrowStmt";
+    case CXCursor_ObjCAtSynchronizedStmt: return "CXCursor_ObjCAtSynchronizedStmt";
+    case CXCursor_ObjCAutoreleasePoolStmt: return "CXCursor_ObjCAutoreleasePoolStmt";
+    case CXCursor_ObjCForCollectionStmt: return "CXCursor_ObjCForCollectionStmt";
+    case CXCursor_CXXCatchStmt: return "CXCursor_CXXCatchStmt";
+    case CXCursor_CXXTryStmt: return "CXCursor_CXXTryStmt";
+    case CXCursor_CXXForRangeStmt: return "CXCursor_CXXForRangeStmt";
+    case CXCursor_SEHTryStmt: return "CXCursor_SEHTryStmt";
+    case CXCursor_SEHExceptStmt: return "CXCursor_SEHExceptStmt";
+    case CXCursor_SEHFinallyStmt: return "CXCursor_SEHFinallyStmt";
+    case CXCursor_NullStmt: return "CXCursor_NullStmt";
+    case CXCursor_DeclStmt: return "CXCursor_DeclStmt";
     }
     return "";
 }
