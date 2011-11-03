7f276b02b8c5963b57f929d38d553320a1ea0475
Anders Bakken <agbakken@gmail.com>
2011-11-03 16:50:21 -0700

    Add some logging and don't save-excursion at the wrong time.

--- rtags.el
+++ rtags.el
@@ -15,6 +15,14 @@
   :type 'boolean
   :group 'rtags)
 
+(defun rtags-log (log)
+  (save-excursion
+    (set-buffer (get-buffer-create "*RTags Log*"))
+    (goto-char (point-max))
+    (insert "**********************************\n" log "\n")
+    )
+  )
+
 (defvar last-rtags-update-process nil)
 (defun rtags-update ()
   (interactive)
@@ -28,6 +36,7 @@
 (defun rtags-goto-location(location)
   (let (line column)
     (string-match "\\(.*\\):\\([0-9]+\\):\\([0-9]+\\)" location)
+;    (message (concat "rtags-goto-location " location (if (match-beginning 1) "yes" "no")))
     (if (match-beginning 1)
         (progn
           (setq line (string-to-int (match-string 2 location)))
@@ -41,27 +50,26 @@
     )
   )
 
-(defun rtags-find-symbol-at-point(&optional p)
+(defun rtags-find-symbol-at-point(&optional pos)
   (interactive)
-  (save-excursion
-    (if p
-        (goto-char p))
-    (let ((bufname (buffer-file-name))
-          (line (int-to-string (line-number-at-pos)))
-          (column nil))
+  (let ((bufname (buffer-file-name))
+        (line (int-to-string (line-number-at-pos pos)))
+        (column nil))
+    (save-excursion
+      (if pos
+          (goto-char pos))
       (if (looking-at "[0-9A-Za-z_~#]")
           (progn
             (while (and (> (point) 1) (looking-at "[0-9A-Za-z_~#]"))
               (backward-char))
             (if (not (looking-at "[0-9A-Za-z_~#]"))
                 (forward-char))
-            (setq column (int-to-string (- (point) (point-at-bol) -1)))))
+            (setq column (int-to-string (- (point) (point-at-bol) -1))))))
     (with-temp-buffer
-      ;; (message (executable-find "rc"))
-      (message (concat (executable-find "rc") " --follow-symbol " bufname ":" line ":" column ":"))
+      (rtags-log (concat (executable-find "rc") " --follow-symbol " bufname ":" line ":" column ":"))
       (call-process (executable-find "rc") nil (list t nil) nil "--follow-symbol" (concat bufname ":" line ":" column ":"))
+      (rtags-log (buffer-string))
       (rtags-goto-location (buffer-string)))
-    )
     )
   )
 
@@ -79,7 +87,9 @@
 (defun rtags-complete (string predicate code)
   (let ((completions))
     (with-temp-buffer
-      (call-process "rc" nil (list t nil) nil "-S" "-n" "-l" string)
+      (rtags-log (concat (executable-find "rc") " -S -n -l " string))
+      (call-process (executable-find "rc") nil (list t nil) nil "-S" "-n" "-l" string)
+      (rtags-log (buffer-string))
       (setq 'completions (split-string (buffer-string) "\n" t)))))
       ;; (all-completion string completions))))
       ;; (cond ((eq code nil)
@@ -104,14 +114,14 @@
             (setq column (int-to-string (- (point) (point-at-bol) -1))))))
     (if (get-buffer "*Rtags-Complete*")
         (kill-buffer "*Rtags-Complete*"))
-    (switch-to-buffer (generate-new-buffer "*Rtags-Complete*"))
-      ;; (message (executable-find "rc"))
-    (message (concat (executable-find "rc") (concat " " mode " " bufname ":" line ":" column ":")))
+    (set-buffer (generate-new-buffer "*Rtags-Complete*"))
+    (rtags-log (concat (executable-find "rc") " " mode " " bufname ":" line ":" column ":"))
     (call-process (executable-find "rc") nil (list t nil) nil mode (concat bufname ":" line ":" column ":"))
+    (rtags-log (buffer-string))
     (if (= (point-min) (point-max))
         (progn
 ;          (kill-buffer "*Rtags-Complete*")
-          (switch-to-buffer previous)
+          (set-buffer previous)
           nil)
       (progn
         (if (= (count-lines (point-min) (point-max)) 1)
@@ -131,7 +141,9 @@
         (setq prompt (concat p ": (default " tagname ") "))
       (setq prompt (concat p ": ")))
     (with-temp-buffer
-      (call-process "rc" nil (list t nil) nil "-l" "")
+      (rtags-log (concat (executable-find "rc") " -l \"\""))
+      (call-process (executable-find "rc") nil (list t nil) nil "-l" "")
+      (rtags-log (buffer-string))
       (setq completions (split-string (buffer-string) "\n" t)))
       ;; (setq completions (split-string "test1" "test1()")))
     (setq input (completing-read prompt completions nil nil nil gtags-history-list))
@@ -140,7 +152,9 @@
     (if (get-buffer "*Rtags-Complete*")
         (kill-buffer "*Rtags-Complete*"))
     (switch-to-buffer (generate-new-buffer "*Rtags-Complete*"))
-    (call-process "rc" nil (list t nil) nil switch tagname)
+    (rtags-log (concat (executable-find "rc") " " switch " " tagname))
+    (call-process (executable-find "rc") nil (list t nil) nil switch tagname)
+    (rtags-log (buffer-string))
     (if (= (point-min) (point-max))
         (progn
 ;          (kill-buffer "*Rtags-Complete*")
