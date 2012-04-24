71f9ade326d1b5ab041e7e3dca6d44c8b71e3f2d
Anders Bakken <agbakken@gmail.com>
2012-04-23 20:27:47 -0700

    Implement dependency status

--- rdm/StatusJob.cpp
+++ rdm/StatusJob.cpp
@@ -21,6 +21,26 @@ void StatusJob::run()
     }
 
     if (query.isEmpty() || query == "dependencies") {
+        LevelDB db;
+        if (db.open(Server::Dependency, LevelDB::ReadOnly)) {
+            ret.append(Server::databaseName(Server::Dependency));
+            leveldb::Iterator* it = db.db()->NewIterator(leveldb::ReadOptions());
+            it->SeekToFirst();
+            char buf[1024];
+            memcpy(buf, "  ", 2);
+            while (it->Valid()) {
+                memcpy(buf + 2, it->key().data(), it->key().size());
+                memcpy(buf + 2 + it->key().size(), " is depended on by:", 20);
+                ret.append(buf);
+                const QSet<Path> deps = Rdm::readValue<QSet<Path> >(it);
+                memcpy(buf + 2, "  ", 2);
+                foreach (const Path &p, deps) {
+                    memcpy(buf + 4, p.constData(), p.size() + 1);
+                    ret.append(buf);
+                }
+                it->Next();
+            }
+        }
     }
 
     if (query.isEmpty() || query == "symbols") {
