3947756fc60ef663534dcbae0a31b1893e935268
Jan Erik Hanssen <jhanssen@gmail.com>
2012-05-04 02:01:00 -0700

    Rewrite the smaps parser to use FILE* instead of QFile

--- rdm/MemoryMonitor.cpp
+++ rdm/MemoryMonitor.cpp
@@ -12,18 +12,50 @@ MemoryMonitor::MemoryMonitor()
 #ifdef Q_OS_LINUX
 static inline quint64 usageLinux()
 {
-    const pid_t pid = getpid();
-    QFile file(("/proc/" + QByteArray::number(pid) + "/smaps"));
-    if (!file.open(QFile::ReadOnly))
-        return 0;
     quint64 total = 0;
-    QList<QByteArray> lines = file.readAll().split('\n');
-    foreach(const QByteArray& line, lines) {
-        if (!qstrncmp("Private_Clean:", line, 14))
-            total += (atoll(line.constData() + 14) * 1024);
-        else if (!qstrncmp("Private_Dirty:", line, 14))
-            total += (atoll(line.constData() + 14) * 1024);
+    const pid_t pid = getpid();
+    FILE* file = fopen(("/proc/" + QByteArray::number(pid) + "/smaps").constData(), "r");
+    if (!file)
+        return 0;
+
+    enum { BufferSize = 4096 };
+    int lastnewline = 0, lastread = BufferSize;
+    char buffer[BufferSize];
+
+    for (;;) {
+        if (lastnewline) {
+            memmove(buffer, buffer + lastnewline + 1, lastread - lastnewline);
+            lastread = lastnewline + 1;
+        }
+
+        lastread = fread(buffer + BufferSize - lastread, 1, lastread, file);
+        if (!lastread)
+            break;
+
+        const char* end = buffer + lastread;
+        char* entry;
+        int nextnewline = 0;
+        for (;;) {
+            entry = buffer + nextnewline;
+            lastnewline = nextnewline;
+            for (char* nl = entry; nl != end; ++nl) {
+                if (*nl == '\n') {
+                    nextnewline = (nl - buffer) + 1;
+                    break;
+                }
+            }
+            if (lastnewline == nextnewline)
+                break;
+
+            if (!strncmp("Private_Clean:", entry, 14))
+                total += (atoll(entry + 14) * 1024);
+            else if (!strncmp("Private_Dirty:", entry, 14))
+                total += (atoll(entry + 14) * 1024);
+        }
     }
+
+    fclose(file);
+
     return total;
 }
 #elif Q_OS_FREEBSD
