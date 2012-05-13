8c2527f0edafe2d00eec660443503e7d6eba81e8
Jan Erik Hanssen <jhanssen@gmail.com>
2012-05-13 01:28:31 -0700

    Black magic

--- shared/PCH_GCC4_v2.cmake
+++ shared/PCH_GCC4_v2.cmake
@@ -20,20 +20,26 @@
 # make clean should delete the pch file
 #
 # example : ADD_PCH_RULE(headers.h myprog_SRCS)
-MACRO (ADD_PCH_RULE  _header_filename _src_list)
+MACRO (ADD_PCH_RULE _header_filename _src_list _cflag_list)
 	SET(_gch_filename "${_header_filename}.gch")
 	LIST(APPEND ${_src_list} ${_gch_filename})
 	SET (_args ${CMAKE_CXX_FLAGS})
+	LIST(APPEND ${_cflag_list} -include ${_header_filename} -Winvalid-pch)
 	LIST(APPEND _args -c ${_header_filename} -o ${_gch_filename})
 	GET_DIRECTORY_PROPERTY(DIRINC INCLUDE_DIRECTORIES)
+	STRING(TOUPPER ${CMAKE_BUILD_TYPE} BUILDTYPE)
+	LIST(APPEND _args ${CMAKE_CXX_FLAGS_${BUILDTYPE}})
 	foreach (_inc ${DIRINC})
 		LIST(APPEND _args "-I" ${_inc})
 	endforeach(_inc ${DIRINC})
+	GET_DIRECTORY_PROPERTY(DIRDEF COMPILE_DEFINITIONS)
+	foreach (_def ${DIRDEF})
+		STRING(REPLACE "\"" "\\\"" _def "${_def}" )
+		LIST(APPEND _args -D${_def})
+	endforeach(_def ${DIRDEF})
 	SEPARATE_ARGUMENTS(_args)
 	add_custom_command(OUTPUT ${_gch_filename}
 		   COMMAND rm -f ${_gch_filename}
 		   COMMAND ${CMAKE_CXX_COMPILER} ${CMAKE_CXX_COMPILER_ARG1} ${_args}
 			    DEPENDS ${_header_filename})
 ENDMACRO(ADD_PCH_RULE _header_filename _src_list)
-
-
