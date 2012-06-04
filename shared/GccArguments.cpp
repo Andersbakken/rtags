Andersbakken/rtags

commit accf6c011f7f7b38e29154e596186f64abda193c

Remove nasty-ass hack

shared/GccArguments.cpp
@@ -21,7 +21,7 @@ GccArguments::GccArguments()
 {
 }
 
-GccArguments::GccArguments(const QByteArray& args, const Path& base)
+GccArguments::GccArguments(const QByteArray &args, const Path &base)
     : mImpl(new GccArgumentsImpl)
 {
     parse(args, base);
@@ -37,7 +37,7 @@ void GccArguments::clear()
     *mImpl = GccArgumentsImpl();
 }
 
-static inline GccArguments::Lang guessLang(const Path& fullPath)
+static inline GccArguments::Lang guessLang(const Path &fullPath)
 {
     QByteArray compiler = fullPath.fileName();
     QByteArray c;
@@ -75,7 +75,7 @@ static inline GccArguments::Lang guessLang(const Path& fullPath)
     return lang;
 }
 
-bool GccArguments::parse(QByteArray args, const Path& base)
+bool GccArguments::parse(QByteArray args, const Path &base)
 {
     mImpl->type = NoType;
     mImpl->lang = NoLang;
@@ -87,8 +87,8 @@ bool GccArguments::parse(QByteArray args, const Path& base)
     QList<QByteArray> split;
     QByteArray old2 = args;
     {
-        char* cur = args.data();
-        char* prev = cur;
+        char *cur = args.data();
+        char *prev = cur;
         // ### handle escaped quotes?
         int size = args.size();
         while (size > 0) {
@@ -213,15 +213,6 @@ bool GccArguments::parse(QByteArray args, const Path& base)
                     } else {
                         arg = cur;
                     }
-                    if (!mImpl->clangArgs.contains(arg)) {
-                        /* This is nasty stuff but can be important for pch. To
-                         * see the effect just reset to this commit~ and notice
-                         * that pch doesn't work for rtags since the pch file
-                         * has different defines than the sources using it, even
-                         * if the difference only is the multiple definition of
-                         * QT_(NO_)DEBUG */
-                        mImpl->clangArgs.append(arg);
-                    }
                 } else if (!strncmp(cur, "-I", 2)) {
                     Path inc;
                     pathok = false;
@@ -292,7 +283,7 @@ QList<QByteArray> GccArguments::inputFiles() const
 QList<QByteArray> GccArguments::explicitIncludes() const
 {
     QList<QByteArray> incs;
-    foreach (const Path& p, mImpl->includes)
+    foreach (const Path &p, mImpl->includes)
         incs.append(p);
     return incs;
 }

shared/GccArguments.h
@@ -14,10 +14,10 @@ public:
     enum Lang { NoLang, C, CPlusPlus };
 
     GccArguments();
-    GccArguments(const QByteArray& args, const Path& base);
+    GccArguments(const QByteArray &args, const Path &base);
     ~GccArguments();
 
-    bool parse(QByteArray args, const Path& base);
+    bool parse(QByteArray args, const Path &base);
     Type type() const;
     Lang lang() const;
     void clear();
@@ -32,7 +32,7 @@ public:
 
 private:
     Q_DISABLE_COPY(GccArguments);
-    GccArgumentsImpl* mImpl;
+    GccArgumentsImpl *mImpl;
 };
 
 #endif

commit f83e5322df2e14668984a3eb8dacf13d94cefda1

Oops. I broke GccArguments in accf6c011f7f7b38e29154e596186f64abda193c

shared/GccArguments.cpp
@@ -216,6 +216,7 @@ bool GccArguments::parse(QByteArray args, const Path &base)
                     } else {
                         arg = cur;
                     }
+                    mImpl->clangArgs.append(arg);
                 } else if (!strncmp(cur, "-I", 2)) {
                     Path inc;
                     pathok = false;