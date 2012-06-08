Andersbakken/rtags

commit 5b0a8c23604cd1a3917ca4cb9ab4f599f32ceded

Various fixes.

rdm/IndexerJob.cpp
@@ -93,8 +93,11 @@ QByteArray IndexerJob::addNamePermutations(const CXCursor &cursor, const Locatio
             break;
         }
         qname = QByteArray(name);
-        if (ret.isEmpty())
+        if (ret.isEmpty()) {
             ret = qname;
+            if (!addToDB)
+                return ret;
+        }
         if (qparam.isEmpty()) {
             qparam.prepend(qname);
             if (addToDB) {
@@ -193,7 +196,7 @@ static inline bool isInteresting(CXCursorKind kind)
 }
 
 CXChildVisitResult IndexerJob::indexVisitor(CXCursor cursor,
-                                            CXCursor parent,
+                                            CXCursor /*parent*/,
                                             CXClientData client_data)
 {
     IndexerJob *job = static_cast<IndexerJob*>(client_data);
@@ -201,7 +204,7 @@ CXChildVisitResult IndexerJob::indexVisitor(CXCursor cursor,
         return CXChildVisit_Break;
 
     if (testLog(Debug))
-        debug() << "indexVisitor" << cursor << parent;
+        debug() << "indexVisitor" << cursor << clang_getCursorReferenced(cursor);
     const CXCursorKind kind = clang_getCursorKind(cursor);
     if (!isInteresting(kind))
         return CXChildVisit_Recurse;
@@ -245,6 +248,9 @@ CXChildVisitResult IndexerJob::indexVisitor(CXCursor cursor,
             if (!clang_equalCursors(clang_getNullCursor(), ref)) {
                 Q_ASSERT(!clang_equalCursors(cursor, ref));
                 refLoc = job->createLocation(ref, 0);
+                if (testLog(Debug)) {
+                    debug() << "Looked up definition for ref" << ref << cursor;
+                }
             }
         }
 
@@ -307,7 +313,8 @@ CXChildVisitResult IndexerJob::processCursor(const Cursor &cursor, const Cursor 
         info.isDefinition = clang_isCursorDefinition(cursor.cursor);
         info.kind = cursor.kind;
         CXString name;
-        if (clang_isReference(cursor.kind)) {
+        const bool isReference = clang_isReference(cursor.kind);
+        if (isReference) {
             name = clang_getCursorSpelling(ref.cursor);
         } else {
             name = clang_getCursorSpelling(cursor.cursor);
@@ -319,8 +326,7 @@ CXChildVisitResult IndexerJob::processCursor(const Cursor &cursor, const Cursor 
             mSymbols.remove(cursor.location);
             return CXChildVisit_Recurse;
         }
-        const bool addToDB = (info.isDefinition || cursor.kind == CXCursor_FunctionDecl);
-        info.symbolName = addNamePermutations(cursor.cursor, cursor.location, addToDB);
+        info.symbolName = addNamePermutations(cursor.cursor, cursor.location, !isReference);
     } else if (info.kind == CXCursor_Constructor && cursor.kind == CXCursor_TypeRef) {
         return CXChildVisit_Recurse;
     }
@@ -547,7 +553,7 @@ IndexerJob::Cursor IndexerJob::findByUSR(const CXCursor &cursor, CXCursorKind ki
         return ret;
     }
 
-    const Str usr = clang_getCursorUSR(cursor);
+    const Str usr(clang_getCursorUSR(cursor));
     if (!usr.length()) {
         const Cursor ret = { clang_getNullCursor(), Location(), CXCursor_FirstInvalid };
         return ret;

rdm/Rdm.cpp
@@ -24,7 +24,7 @@ QByteArray cursorToString(CXCursor cursor)
     clang_getSpellingLocation(loc, &file, &line, &col, &off);
     const QByteArray fileName = eatString(clang_getFileName(file));
     if (!fileName.isEmpty()) {
-        ret += " " + fileName + ':' + QByteArray::number(line) + ":" + QByteArray::number(col) + ": (" + QByteArray::number(off) + ") " + eatString(clang_getCursorUSR(cursor));
+        ret += " " + fileName + ':' + QByteArray::number(line) + ":" + QByteArray::number(col) + ": (" + QByteArray::number(off) + ")"; // + eatString(clang_getCursorUSR(cursor));
     }
     return ret;
 }
@@ -61,7 +61,6 @@ CursorInfo findCursorInfo(Database *db, const Location &location, Location *loc)
     }
     if (!found && it->isValid()) {
         const Slice key = it->key();
-        debug() << "key" << key << "needle" << needle;
         const Location loc = Location::fromKey(key.data());
         if (location.fileId() == loc.fileId()) {
             const int off = location.offset() - loc.offset();
@@ -275,7 +274,6 @@ int dirty(const QSet<quint32> &dirtyFileIds)
             const Location loc = Location::fromKey(key.data());
             // debug() << "looking at" << key;
             if (dirtyFileIds.contains(loc.fileId())) {
-                debug() << "key is dirty. removing" << key;
                 db->remove(key);
             } else {
                 CursorInfo cursorInfo = it->value<CursorInfo>();
@@ -285,7 +283,6 @@ int dirty(const QSet<quint32> &dirtyFileIds)
                     //     debug() << "CursorInfo is empty now. removing" << key;
                     //     db->remove(key);
                     // } else {
-                    debug() << "CursorInfo is modified. Changing" << key;
                     db->setValue<CursorInfo>(key, cursorInfo);
                     ++ret;
                     // }

rdm/Server.cpp
@@ -176,9 +176,7 @@ bool Server::init(const Options &options)
 
     mIndexer = new Indexer(sBase, this);
     connect(mIndexer, SIGNAL(indexingDone(int)), this, SLOT(onIndexingDone(int)));
-    connect(mIndexer, SIGNAL(jobsComplete()), this, SLOT(onSymbolNamesChanged()));
 
-    onSymbolNamesChanged();
     return true;
 }
 
@@ -573,12 +571,6 @@ void Server::startJob(Job *job)
     connect(job, SIGNAL(complete(int)), this, SLOT(onComplete(int)));
     connect(job, SIGNAL(output(int, QByteArray)), this, SLOT(onOutput(int, QByteArray)));
     QThreadPool::globalInstance()->start(job, job->priority());
-}
-void Server::onSymbolNamesChanged()
-{
-    MatchJob *match = MatchJob::createCompletionMatchJob();
-    mCachedSymbolNames.clear();
-    startJob(match);
 }
 
 ScopedDB::ScopedDB(Database *db, LockType lockType)

rdm/Server.h
@@ -77,7 +77,6 @@ public:
 signals:
     void complete(int id, const QList<QByteArray>& locations);
 private slots:
-    void onSymbolNamesChanged();
     void onNewConnection();
     void onNewMessage(Message* message);
     void onIndexingDone(int id);

shared/Log.cpp
@@ -15,9 +15,12 @@ static int sLevel = 0;
 
 static void cleanupSinks()
 {
-    QMutexLocker lock(&sOutputsMutex);
-    qDeleteAll(sOutputs);
-    sOutputs.clear();
+    QSet<Output*> copy;
+    {
+        QMutexLocker lock(&sOutputsMutex);
+        qSwap(copy, sOutputs);
+    }
+    qDeleteAll(copy);
 }
 
 class FileOutput : public Output
@@ -214,8 +217,9 @@ Output::Output(int logLevel)
     : mLogLevel(logLevel)
 {
     QMutexLocker lock(&sOutputsMutex);
-    if (sOutputs.isEmpty())
+    if (sOutputs.isEmpty()) {
         qAddPostRoutine(cleanupSinks);
+    }
     sOutputs.insert(this);
 }
 Output::~Output()

commit f4ad3a3e2ec0df8f5e5177e2ff6028e441c1cfcb

5b0a8c23604cd1a3917ca4cb9ab4f599f32ceded broke the addNamePermutations stuff a little. Fix it again

rdm/IndexerJob.cpp
@@ -80,12 +80,30 @@ QByteArray IndexerJob::addNamePermutations(const CXCursor &cursor, const Locatio
 
     CXCursor cur = cursor, null = clang_getNullCursor();
     CXCursorKind kind;
+    bool first = true;
     for (;;) {
         if (clang_equalCursors(cur, null))
             break;
         kind = clang_getCursorKind(cur);
-        if (clang_isTranslationUnit(kind))
-            break;
+        if (first) {
+            first = false;
+        } else {
+            bool ok = false;
+            switch (kind) {
+            case CXCursor_Namespace:
+            case CXCursor_ClassDecl:
+            case CXCursor_StructDecl:
+            case CXCursor_CXXMethod:
+            case CXCursor_Constructor:
+            case CXCursor_FunctionDecl:
+                ok = true;
+                break;
+            default:
+                break;
+            }
+            if (!ok)
+                break;
+        }
 
         CXStringScope displayName(clang_getCursorDisplayName(cur));
         const char *name = clang_getCString(displayName.string);