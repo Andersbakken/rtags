e363fc1f46fe5082075972cada94ff3f5d6ec445
Jan Erik Hanssen <jhanssen@gmail.com>
2012-06-20 14:54:12 -0700

    Make mExtraFlags non-const

--- src/MakefileParser.h
+++ src/MakefileParser.h
@@ -39,7 +39,7 @@ private:
     QProcess *mProc;
     ByteArray mData;
     DirectoryTracker *mTracker;
-    const List<ByteArray> mExtraFlags;
+    List<ByteArray> mExtraFlags;
     Map<ByteArray, ByteArray> mPchs;
 };
 
