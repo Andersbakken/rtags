03ac29a13c3d4a79e71d03d18fa64c7f08b9ddbc
Anders Bakken <agbakken@gmail.com>
2012-07-02 14:10:50 -0700

    Process define definitions explicitly. Since we never get these cursors indexed as the cursor argument in indexVisitor we have to do when we see it on the macro expansion arguments.

--- src/IndexerJob.cpp
+++ src/IndexerJob.cpp
@@ -407,6 +407,11 @@ CXChildVisitResult IndexerJob::processCursor(const Cursor &cursor, const Cursor 
         }
         return CXChildVisit_Recurse;
     }
+    if (cursor.kind == CXCursor_MacroExpansion
+        && ref.kind == CXCursor_MacroDefinition
+        && mSymbols.contains(ref.location)) {
+        processCursor(ref, ref);
+    }
     const bool refOk = (!clang_isInvalid(ref.kind) && !ref.location.isNull() && ref.location != cursor.location);
     if (refOk && !isInteresting(ref.kind))
         return CXChildVisit_Recurse;
