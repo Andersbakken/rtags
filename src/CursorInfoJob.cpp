e65ad34e0284736e4943b9a7c246124777b7219e
Anders Bakken <agbakken@gmail.com>
2012-07-13 12:25:51 -0700

    Nicer CursorInfoJob. Print refs.

--- src/CursorInfoJob.cpp
+++ src/CursorInfoJob.cpp
@@ -18,16 +18,27 @@ void CursorInfoJob::execute()
 {
     ScopedDB db = Server::instance()->db(Server::Symbol, ReadWriteLock::Read);
     Location found;
-    const CursorInfo cursorInfo = RTags::findCursorInfo(db, location, &found);
+    const CursorInfo ci = RTags::findCursorInfo(db, location, &found);
     if (isAborted())
         return;
-    if (cursorInfo.symbolLength) {
+    if (ci.symbolLength) {
         char buf[1024];
-        const CXStringScope kind(clang_getCursorKindSpelling(cursorInfo.kind));
-        const int w = snprintf(buf, sizeof(buf), "%s symbolName: %s kind: %s isDefinition: %s symbolLength: %d",
-                               found.key(flags).constData(), cursorInfo.symbolName.constData(),
-                               clang_getCString(kind.string), cursorInfo.isDefinition ? "true" : "false",
-                               cursorInfo.symbolLength);
+        const CXStringScope kind(clang_getCursorKindSpelling(ci.kind));
+        const int w = snprintf(buf, sizeof(buf), "%s symbolName: %s kind: %s isDefinition: %s symbolLength: %d%s",
+                               found.key(flags).constData(), ci.symbolName.constData(),
+                               clang_getCString(kind.string), ci.isDefinition ? "true" : "false",
+                               ci.symbolLength,
+                               (ci.references.isEmpty() && ci.additionalReferences.isEmpty() ? "" : " references:"));
         write(ByteArray(buf, w));
+        for (Set<Location>::const_iterator rit = ci.references.begin(); rit != ci.references.end(); ++rit) {
+            const Location &l = *rit;
+            snprintf(buf, sizeof(buf), "    %s", l.key().constData());
+            write(buf);
+        }
+        for (Set<Location>::const_iterator rit = ci.additionalReferences.begin(); rit != ci.additionalReferences.end(); ++rit) {
+            const Location &l = *rit;
+            snprintf(buf, sizeof(buf), "    %s (additional)", l.key().constData());
+            write(buf);
+        }
     }
 }
