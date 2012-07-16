eb12ec84cb3b7035a9edd10101c7e3051c717c68
Anders Bakken <agbakken@gmail.com>
2012-07-15 21:48:42 -0700

    Kill ###. Not sure it's right.

--- src/ThreadLocal.h
+++ src/ThreadLocal.h
@@ -3,7 +3,6 @@
 
 #include <pthread.h>
 
-// ### the const usage in this class is a bit hacky
 template<typename T>
 class ThreadLocal
 {
@@ -18,8 +17,8 @@ public:
     T& get() { return *reinterpret_cast<T*>(getData()); }
     const T& get() const { return *reinterpret_cast<const T*>(getData()); }
 
-    operator const T& () { return get(); }
-
+    operator const T& () const { return get(); }
+    operator T& () { return get(); }
 private:
     void ensureKey() const { if (mExists) return; mExists = true; pthread_key_create(&mKey, 0); }
     void setData(void* data) { ensureKey(); pthread_setspecific(mKey, data); }
