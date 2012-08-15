06a726fdec02bdf70aa4869c94b4a8c7d3e35a08
Anders Bakken <agbakken@gmail.com>
2012-08-15 11:01:37 -0700

    More support for grtags in statusjob

--- src/StatusJob.cpp
+++ src/StatusJob.cpp
@@ -51,123 +51,162 @@ void StatusJob::execute()
         }
     }
 
-    const char *alternatives = "general|fileids|dependencies|fileinfos|symbols|symbolnames|pch|visitedfiles";
+    const char *alternatives = "general|fileids|dependencies|fileinfos|symbols|symbolnames|pch|visitedfiles|grfiles|gr";
     shared_ptr<Project> proj = project();
-    if (!proj || !proj->indexer) {
+    if (!proj) {
         if (!matched) {
             write(alternatives);
         }
         return;
     }
 
-    if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "dependencies")) {
-        matched = true;
-        ScopedDB database = db(Project::Dependency, ReadWriteLock::Read);
-        write(delimiter);
-        write(proj->databaseDir(Project::Dependency));
-        RTags::Ptr<Iterator> it(database->createIterator());
-        Map<uint32_t, Set<uint32_t> > depsReversed;
-        it->seekToFirst();
-        char buf[1024];
-        while (it->isValid()) {
-            if (isAborted())
-                return;
-            const uint32_t key = *reinterpret_cast<const uint32_t*>(it->key().data());
-            snprintf(buf, sizeof(buf), "  %s (%d) is depended on by", Location::path(key).constData(), key);
-            write(buf);
-            const Set<uint32_t> deps = it->value<Set<uint32_t> >();
-            for (Set<uint32_t>::const_iterator dit = deps.begin(); dit != deps.end(); ++dit) {
-                snprintf(buf, sizeof(buf), "    %s (%d)", Location::path(*dit).constData(), *dit);
+    if (proj->indexer) {
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "dependencies")) {
+            matched = true;
+            ScopedDB database = db(Project::Dependency, ReadWriteLock::Read);
+            write(delimiter);
+            write(proj->databaseDir(Project::Dependency));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            Map<uint32_t, Set<uint32_t> > depsReversed;
+            it->seekToFirst();
+            char buf[1024];
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+                const uint32_t key = *reinterpret_cast<const uint32_t*>(it->key().data());
+                snprintf(buf, sizeof(buf), "  %s (%d) is depended on by", Location::path(key).constData(), key);
                 write(buf);
-                depsReversed[*dit].insert(key);
+                const Set<uint32_t> deps = it->value<Set<uint32_t> >();
+                for (Set<uint32_t>::const_iterator dit = deps.begin(); dit != deps.end(); ++dit) {
+                    snprintf(buf, sizeof(buf), "    %s (%d)", Location::path(*dit).constData(), *dit);
+                    write(buf);
+                    depsReversed[*dit].insert(key);
+                }
+                it->next();
             }
-            it->next();
+            for (Map<uint32_t, Set<uint32_t> >::const_iterator it = depsReversed.begin(); it != depsReversed.end(); ++it) {
+                snprintf(buf, sizeof(buf), "  %s (%d) depends on", Location::path(it->first).constData(), it->first);
+                write(buf);
+                const Set<uint32_t> &deps = it->second;
+                for (Set<uint32_t>::const_iterator dit = deps.begin(); dit != deps.end(); ++dit) {
+                    snprintf(buf, sizeof(buf), "    %s (%d)", Location::path(*dit).constData(), *dit);
+                    write(buf);
+                }
+            }
         }
-        for (Map<uint32_t, Set<uint32_t> >::const_iterator it = depsReversed.begin(); it != depsReversed.end(); ++it) {
-            snprintf(buf, sizeof(buf), "  %s (%d) depends on", Location::path(it->first).constData(), it->first);
-            write(buf);
-            const Set<uint32_t> &deps = it->second;
-            for (Set<uint32_t>::const_iterator dit = deps.begin(); dit != deps.end(); ++dit) {
-                snprintf(buf, sizeof(buf), "    %s (%d)", Location::path(*dit).constData(), *dit);
+
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "symbols")) {
+            matched = true;
+            ScopedDB database = db(Project::Symbol, ReadWriteLock::Read);
+            write(delimiter);
+            write(project()->databaseDir(Project::Symbol));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            it->seekToFirst();
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+                const CursorInfo ci = it->value<CursorInfo>();
+                const Location loc = Location::fromKey(it->key().data());
+                write(loc, ci);
+                it->next();
+            }
+        }
+
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "symbolnames")) {
+            matched = true;
+            ScopedDB database = db(Project::SymbolName, ReadWriteLock::Read);
+            write(delimiter);
+            write(project()->databaseDir(Project::SymbolName));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            it->seekToFirst();
+            char buf[1024];
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+                snprintf(buf, sizeof(buf), "  %s:", it->key().byteArray().constData());
+                write(buf);
+                const Set<Location> locations = it->value<Set<Location> >();
+                for (Set<Location>::const_iterator lit = locations.begin(); lit != locations.end(); ++lit) {
+                    const Location &loc = *lit;
+                    snprintf(buf, sizeof(buf), "    %s", loc.key().constData());
+                    write(buf);
+                }
+                it->next();
+            }
+        }
+
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "fileinfos")) {
+            matched = true;
+            ScopedDB database = db(Project::FileInformation, ReadWriteLock::Read);
+            write(delimiter);
+            write(project()->databaseDir(Project::FileInformation));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            it->seekToFirst();
+            char buf[1024];
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+
+                const FileInformation fi = it->value<FileInformation>();
+                const uint32_t fileId = *reinterpret_cast<const uint32_t*>(it->key().data());
+                snprintf(buf, 1024, "  %s: last compiled: %s compile args: %s",
+                         Location::path(fileId).constData(),
+                         RTags::timeToString(fi.lastTouched).constData(),
+                         ByteArray::join(fi.compileArgs, " ").constData());
+                write(buf);
+                it->next();
+            }
+        }
+
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "visitedfiles")) {
+            matched = true;
+            write(delimiter);
+            write("visitedfiles");
+            char buf[1024];
+            const Set<uint32_t> visitedFiles = proj->indexer->visitedFiles();
+
+            for (Set<uint32_t>::const_iterator it = visitedFiles.begin(); it != visitedFiles.end(); ++it) {
+                const uint32_t id = *it;
+                snprintf(buf, sizeof(buf), "  %s: %d", Location::path(id).constData(), id);
                 write(buf);
             }
         }
     }
-
-    if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "symbols")) {
-        matched = true;
-        ScopedDB database = db(Project::Symbol, ReadWriteLock::Read);
-        write(delimiter);
-        write(project()->databaseDir(Project::Symbol));
-        RTags::Ptr<Iterator> it(database->createIterator());
-        it->seekToFirst();
-        while (it->isValid()) {
-            if (isAborted())
-                return;
-            const CursorInfo ci = it->value<CursorInfo>();
-            const Location loc = Location::fromKey(it->key().data());
-            write(loc, ci);
-            it->next();
-        }
-    }
-
-    if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "symbolnames")) {
-        matched = true;
-        ScopedDB database = db(Project::SymbolName, ReadWriteLock::Read);
-        write(delimiter);
-        write(project()->databaseDir(Project::SymbolName));
-        RTags::Ptr<Iterator> it(database->createIterator());
-        it->seekToFirst();
-        char buf[1024];
-        while (it->isValid()) {
-            if (isAborted())
-                return;
-            snprintf(buf, sizeof(buf), "  %s:", it->key().byteArray().constData());
-            write(buf);
-            const Set<Location> locations = it->value<Set<Location> >();
-            for (Set<Location>::const_iterator lit = locations.begin(); lit != locations.end(); ++lit) {
-                const Location &loc = *lit;
-                snprintf(buf, sizeof(buf), "    %s", loc.key().constData());
-                write(buf);
+    if (proj->grtags) {
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "grfiles")) {
+            matched = true;
+            ScopedDB database = db(Project::GRFiles, ReadWriteLock::Read);
+            write(delimiter);
+            write(project()->databaseDir(Project::GRFiles));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            it->seekToFirst();
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+                write(it->key().byteArray());
+                it->next();
             }
-            it->next();
         }
-    }
-
-    if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "fileinfos")) {
-        matched = true;
-        ScopedDB database = db(Project::FileInformation, ReadWriteLock::Read);
-        write(delimiter);
-        write(project()->databaseDir(Project::FileInformation));
-        RTags::Ptr<Iterator> it(database->createIterator());
-        it->seekToFirst();
-        char buf[1024];
-        while (it->isValid()) {
-            if (isAborted())
-                return;
-
-            const FileInformation fi = it->value<FileInformation>();
-            const uint32_t fileId = *reinterpret_cast<const uint32_t*>(it->key().data());
-            snprintf(buf, 1024, "  %s: last compiled: %s compile args: %s",
-                     Location::path(fileId).constData(),
-                     RTags::timeToString(fi.lastTouched).constData(),
-                     ByteArray::join(fi.compileArgs, " ").constData());
-            write(buf);
-            it->next();
-        }
-    }
-
-    if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "visitedfiles")) {
-        matched = true;
-        write(delimiter);
-        write("visitedfiles");
-        char buf[1024];
-        const Set<uint32_t> visitedFiles = proj->indexer->visitedFiles();
-
-        for (Set<uint32_t>::const_iterator it = visitedFiles.begin(); it != visitedFiles.end(); ++it) {
-            const uint32_t id = *it;
-            snprintf(buf, sizeof(buf), "  %s: %d", Location::path(id).constData(), id);
-            write(buf);
+        if (query.isEmpty() || !strcasecmp(query.nullTerminated(), "gr")) {
+            matched = true;
+            ScopedDB database = db(Project::GR, ReadWriteLock::Read);
+            write(delimiter);
+            write(project()->databaseDir(Project::GR));
+            RTags::Ptr<Iterator> it(database->createIterator());
+            it->seekToFirst();
+            char buf[1024];
+            while (it->isValid()) {
+                if (isAborted())
+                    return;
+                snprintf(buf, sizeof(buf), "  %s:", it->key().byteArray().constData());
+                write(buf);
+                const Map<Location, bool> locations = it->value<Map<Location, bool> >();
+                for (Map<Location, bool>::const_iterator lit = locations.begin(); lit != locations.end(); ++lit) {
+                    snprintf(buf, sizeof(buf), "    %s%s", lit->first.key().constData(), lit->second ? "ref" : "");
+                    write(buf);
+                }
+                it->next();
+            }
         }
     }
     if (!matched) {
