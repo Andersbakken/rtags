7ea136860ceec4ac0b104b108151d1c9262db77b
Anders Bakken <agbakken@gmail.com>
2012-10-04 16:51:21 -0700

    Rewrite CursorInfo::allReferences again. I should consider just putting auxilliary references into separate containers.

--- src/CursorInfo.cpp
+++ src/CursorInfo.cpp
@@ -120,42 +120,78 @@ SymbolMap CursorInfo::callers(const Location &loc, const SymbolMap &map) const
     return ret;
 }
 
-static inline void allImpl(const SymbolMap &map, const Location &loc, const CursorInfo &info, SymbolMap &out, bool recurse)
+enum Mode {
+    ClassRefs,
+    VirtualRefs,
+    NormalRefs
+};
+
+static inline void allImpl(const SymbolMap &map, const Location &loc, const CursorInfo &info, SymbolMap &out, Mode mode, CXCursorKind kind)
 {
-    assert(!out.contains(loc));
+    if (out.contains(loc))
+        return;
     out[loc] = info;
     typedef SymbolMap (CursorInfo::*Function)(const SymbolMap &map) const;
-    Function functions[] = { &CursorInfo::referenceInfos, &CursorInfo::targetInfos };
-    for (unsigned i=0; i<sizeof(functions) / sizeof(Function); ++i) {
-        const SymbolMap ret = (info.*functions[i])(map);
-        for (SymbolMap::const_iterator r = ret.begin(); r != ret.end(); ++r) {
-            if (!out.contains(r->first)) {
-                if (recurse) {
-                    allImpl(map, r->first, r->second, out, recurse);
-                } else if (i == 0 || r->second.kind == info.kind) {
-                    out[r->first] = r->second;
-                }
+    const SymbolMap targets = info.targetInfos(map);
+    for (SymbolMap::const_iterator t = targets.begin(); t != targets.end(); ++t) {
+        bool ok;
+        switch (mode) {
+        case VirtualRefs:
+        case NormalRefs:
+            ok = (t->second.kind == kind);
+            break;
+        case ClassRefs:
+            ok = (t->second.isClass()
+                  || t->second.kind == CXCursor_Destructor
+                  || t->second.kind == CXCursor_Constructor);
+            break;
+        }
+        if (ok)
+            allImpl(map, t->first, t->second, out, mode, kind);
+    }
+    const SymbolMap refs = info.referenceInfos(map);
+    for (SymbolMap::const_iterator r = refs.begin(); r != refs.end(); ++r) {
+        switch (mode) {
+        case NormalRefs:
+            out[r->first] = r->second;
+            break;
+        case VirtualRefs:
+            if (r->second.kind == kind) {
+                allImpl(map, r->first, r->second, out, mode, kind);
+            } else {
+                out[r->first] = r->second;
+            }
+            break;
+        case ClassRefs:
+            if (info.isClass()) // for class/struct we want the references inserted directly regardless and also recursed
+                out[r->first] = r->second;
+            if (r->second.isClass()
+                || r->second.kind == CXCursor_Destructor
+                || r->second.kind == CXCursor_Constructor) { // if is a constructor/destructor/class reference we want to recurse it
+                allImpl(map, r->first, r->second, out, mode, kind);
             }
         }
     }
 }
 
-
 SymbolMap CursorInfo::allReferences(const Location &loc, const SymbolMap &map) const
 {
     SymbolMap ret;
-    bool recurse = false;
+    Mode mode = NormalRefs;
     switch (kind) {
     case CXCursor_Constructor:
     case CXCursor_Destructor:
-        recurse = true;
+        mode = ClassRefs;
+        break;
+    case CXCursor_CXXMethod:
+        mode = VirtualRefs;
         break;
     default:
-        recurse = isClass();
+        mode = isClass() ? ClassRefs : VirtualRefs;
         break;
     }
 
-    allImpl(map, loc, *this, ret, recurse);
+    allImpl(map, loc, *this, ret, mode, kind);
     return ret;
 }
 
