29b138b7dd4ab3cdc8f1fa4352cb2190ff2d8249
Anders Bakken <agbakken@gmail.com>
2012-10-03 23:04:26 -0700

    Make pch depend on clang-c/Index.h

--- src/PCH_GCC4_v2.cmake
+++ src/PCH_GCC4_v2.cmake
@@ -63,6 +63,7 @@ MACRO (ADD_PCH_RULE _header_filename _src_list _cflag_list)
             Set.h
             SourceInformation.h
             WaitCondition.h
-            WriteLocker.h)
+            WriteLocker.h
+            ../3rdparty/clang_install/include/clang-c/Index.h)
   set_source_files_properties(${${_src_list}} PROPERTIES OBJECT_DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${_header_filename})
 ENDMACRO()
