76cf98d1a8b5dc88336e733aecdd86e0d6c8375d
Jan Erik Hanssen <jhanssen@gmail.com>
2012-10-16 20:42:56 -0700

    Fix ThreadLocal, the previous implementation was't safe

--- src/ThreadLocal.h
+++ src/ThreadLocal.h
@@ -2,6 +2,8 @@
 #define THREADLOCAL_H
 
 #include <pthread.h>
+#include "Mutex.h"
+#include "MutexLocker.h"
 
 template<typename T>
 class ThreadLocal
@@ -11,22 +13,59 @@ public:
     ThreadLocal(const T& t) : mExists(false) { set(t); }
     ~ThreadLocal() { clear(); }
 
-    void clear() { if (!mExists) return; delete reinterpret_cast<T*>(getData()); pthread_key_delete(mKey); mExists = false; }
+    void clear();
 
-    void set(const T& t) { clear(); setData(new T(t)); }
+    void set(const T& t) { setData(new T(t)); }
+    void remove() { setData(0); }
     T& get() { return *reinterpret_cast<T*>(getData()); }
     const T& get() const { return *reinterpret_cast<const T*>(getData()); }
 
     operator const T& () const { return get(); }
     operator T& () { return get(); }
+
 private:
-    void ensureKey() const { if (mExists) return; mExists = true; pthread_key_create(&mKey, 0); }
-    void setData(void* data) { ensureKey(); pthread_setspecific(mKey, data); }
-    void* getData() const { if (!mExists) { ensureKey(); T* null = new T(); pthread_setspecific(mKey, null); return null; } return pthread_getspecific(mKey); }
+    void setData(void* data)
+    {
+        MutexLocker locker(&mMutex);
+        if (mExists) {
+            delete reinterpret_cast<T*>(getData());
+        } else {
+            mExists = true;
+            pthread_key_create(&mKey, deleteValue);
+        }
+        pthread_setspecific(mKey, data);
+    }
+    void* getData() const
+    {
+        MutexLocker locker(&mMutex);
+        if (!mExists) {
+            mExists = true;
+            pthread_key_create(&mKey, 0);
+            T* empty = new T();
+            pthread_setspecific(mKey, empty);
+            return empty;
+        }
+        return pthread_getspecific(mKey);
+    }
+    static void deleteValue(void* val)
+    {
+        delete reinterpret_cast<T*>(val);
+    }
 
 private:
     mutable bool mExists;
+    mutable Mutex mMutex;
     mutable pthread_key_t mKey;
 };
+
+template<typename T>
+inline void ThreadLocal<T>::clear()
+{
+    MutexLocker locker(&mMutex);
+    if (!mExists)
+        return;
+    pthread_key_delete(mKey);
+    mExists = false;
+}
 
 #endif
