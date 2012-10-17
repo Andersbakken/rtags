c5cbd0e3fdfc2c6d210965cc0a8d3fa87d93b448
Jan Erik Hanssen <jhanssen@gmail.com>
2012-10-16 20:49:28 -0700

    Simplify ThreadLocal

--- src/ThreadLocal.h
+++ src/ThreadLocal.h
@@ -2,18 +2,16 @@
 #define THREADLOCAL_H
 
 #include <pthread.h>
-#include "Mutex.h"
-#include "MutexLocker.h"
 
 template<typename T>
 class ThreadLocal
 {
 public:
-    ThreadLocal() : mExists(false) {}
-    ThreadLocal(const T& t) : mExists(false) { set(t); }
+    ThreadLocal() { init(); }
+    ThreadLocal(const T& t) { init(); set(t); }
     ~ThreadLocal() { clear(); }
 
-    void clear();
+    void clear() { pthread_key_delete(mKey); }
 
     void set(const T& t) { setData(new T(t)); }
     void remove() { setData(0); }
@@ -24,27 +22,17 @@ public:
     operator T& () { return get(); }
 
 private:
+    void init()
+    {
+        pthread_key_create(&mKey, deleteValue);
+    }
     void setData(void* data)
     {
-        MutexLocker locker(&mMutex);
-        if (mExists) {
-            delete reinterpret_cast<T*>(getData());
-        } else {
-            mExists = true;
-            pthread_key_create(&mKey, deleteValue);
-        }
+        delete reinterpret_cast<T*>(getData());
         pthread_setspecific(mKey, data);
     }
     void* getData() const
     {
-        MutexLocker locker(&mMutex);
-        if (!mExists) {
-            mExists = true;
-            pthread_key_create(&mKey, 0);
-            T* empty = new T();
-            pthread_setspecific(mKey, empty);
-            return empty;
-        }
         return pthread_getspecific(mKey);
     }
     static void deleteValue(void* val)
@@ -53,19 +41,7 @@ private:
     }
 
 private:
-    mutable bool mExists;
-    mutable Mutex mMutex;
     mutable pthread_key_t mKey;
 };
-
-template<typename T>
-inline void ThreadLocal<T>::clear()
-{
-    MutexLocker locker(&mMutex);
-    if (!mExists)
-        return;
-    pthread_key_delete(mKey);
-    mExists = false;
-}
 
 #endif
