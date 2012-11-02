b03758309e7635099f00b7c7d5303dda5e530622
Anders Bakken <agbakken@gmail.com>
2012-11-02 02:04:17 -0700

    Avoid the .. in the defines.

--- src/rtags.cmake
+++ src/rtags.cmake
@@ -3,7 +3,7 @@ include(clang.cmake)
 include(PCH_GCC4_v2.cmake)
 
 include_directories(
-    ${CMAKE_CURRENT_SOURCE_DIR}../3rdparty/picojson
+    ${PROJECT_SOURCE_DIR}/3rdparty/picojson
     )
 
 set(rtags_client_HDRS
