2faf3929ae578aa98943922322e9ec9cb2b8fb0a
Anders Bakken <agbakken@gmail.com>
2012-11-02 01:34:22 -0700

    Fix deadlock.

--- src/ValidateDBJob.cpp
+++ src/ValidateDBJob.cpp
@@ -15,33 +15,34 @@ void ValidateDBJob::execute()
     int errors = 0;
     int total = 0;
     Set<Location> newErrors;
-
-    Scope<const SymbolMap&> scope = project()->lockSymbolsForRead();
-    if (scope.isNull())
-        return;
-    const SymbolMap &map = scope.data();
-    for (SymbolMap::const_iterator it = map.begin(); it != map.end(); ++it) {
-        if (isAborted()) {
+    {
+        Scope<const SymbolMap&> scope = project()->lockSymbolsForRead();
+        if (scope.isNull())
             return;
-        }
-        const CursorInfo &ci = it->second;
-        if (!ci.symbolLength) {
-            const Location &loc = it->first;
-            if (!mPrevious.contains(loc)) {
-                Log stream(Error);
-                stream << "Invalid entry for " << loc
-                       << " symbolName: " << ci.symbolName;
-                if (ci.kind)
-                    stream << " kind: " << RTags::eatString(clang_getCursorKindSpelling(ci.kind));// this somehow seems to hang when kind == 0
-                stream << " isDefinition: " << (ci.isDefinition ? "true" : "false")
-                       << " target: " << ci.targets
-                       << " references:";
-                for (Set<Location>::const_iterator rit = ci.references.begin(); rit != ci.references.end(); ++rit) {
-                    stream << " " << *rit;
-                }
+        const SymbolMap &map = scope.data();
+        for (SymbolMap::const_iterator it = map.begin(); it != map.end(); ++it) {
+            if (isAborted()) {
+                return;
             }
-            newErrors.insert(loc);
-            ++errors;
+            const CursorInfo &ci = it->second;
+            if (!ci.symbolLength) {
+                const Location &loc = it->first;
+                if (!mPrevious.contains(loc)) {
+                    Log stream(Error);
+                    stream << "Invalid entry for " << loc
+                           << " symbolName: " << ci.symbolName;
+                    if (ci.kind)
+                        stream << " kind: " << RTags::eatString(clang_getCursorKindSpelling(ci.kind));// this somehow seems to hang when kind == 0
+                    stream << " isDefinition: " << (ci.isDefinition ? "true" : "false")
+                           << " target: " << ci.targets
+                           << " references:";
+                    for (Set<Location>::const_iterator rit = ci.references.begin(); rit != ci.references.end(); ++rit) {
+                        stream << " " << *rit;
+                    }
+                }
+                newErrors.insert(loc);
+                ++errors;
+            }
         }
     }
     mErrors(newErrors);
