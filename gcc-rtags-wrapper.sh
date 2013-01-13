3055288045e2f7edd645fb081b35d16ec419be32
Anders Bakken <agbakken@gmail.com>
2013-01-13 10:29:14 -0800

    rename

--- gcc-rtags-wrapper.sh
+++ gcc-rtags-wrapper.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-test -x "`which gcc-rtags-hook.sh`" && gcc-rtags-hook.sh $0 "$@" &
+function followSymlink()
+{
+    python -c "import os, sys; print os.path.realpath(\"$1\")"
+}
+
+hook=`followSymlink $BASH_SOURCE | sed -e 's,gcc-wrapper.sh$,gcc-rtags-hook.sh,'`
+test -x "$hook" && "$hook" $0 "$@" &
 
 if [ -z "$GCC_WRAPPER_BYPASS" ]; then
     which -a "`basename $0`" | while read i; do
-        resolved=`python -c "import os, sys; print os.path.realpath(\"$i\")"`
+        resolved=`followSymlink $i`
         if [ `basename $resolved` != "gcc-wrapper.sh" ]; then
             $i "$@"
             exit $?
