9582b470d27a422dcde34b061ca0684ce24311e8
Anders Bakken <agbakken@gmail.com>
2013-01-13 11:22:09 -0800

    Oops.

--- gcc-rtags-wrapper.sh
+++ gcc-rtags-wrapper.sh
@@ -5,13 +5,13 @@ function followSymlink()
     python -c "import os, sys; print os.path.realpath(\"$1\")"
 }
 
-hook=`followSymlink $BASH_SOURCE | sed -e 's,gcc-wrapper.sh$,gcc-rtags-hook.sh,'`
+hook=`followSymlink $BASH_SOURCE | sed -e 's,gcc-rtags-wrapper.sh$,gcc-rtags-hook.sh,'`
 test -x "$hook" && "$hook" $0 "$@" &
 
 if [ -z "$GCC_WRAPPER_BYPASS" ]; then
     which -a "`basename $0`" | while read i; do
         resolved=`followSymlink $i`
-        if [ `basename $resolved` != "gcc-wrapper.sh" ]; then
+        if [ `basename $resolved` != "gcc-rtags-wrapper.sh" ]; then
             $i "$@"
             exit $?
         fi
