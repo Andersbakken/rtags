a86cbdad60fe64b79fac40bff0dcdf2bc7fa624e
Jan Erik Hanssen <jhanssen@gmail.com>
2013-03-11 12:01:31 -0700

    Store rc before modifying PATH

--- bin/gcc-rtags-wrapper.sh
+++ bin/gcc-rtags-wrapper.sh
@@ -5,14 +5,15 @@ function followSymlink()
     python -c "import os, sys; print os.path.realpath(\"$1\")"
 }
 
+rc=`which rc`
 for i in `which -a "\`basename $0\`"`; do
     resolved=`followSymlink $i`
     if [ `basename $resolved` != "gcc-rtags-wrapper.sh" ]; then
-        if [ -z "$RTAGS_DISABLED" ] && [ -x "`which rc`" ]; then
+        if [ -z "$RTAGS_DISABLED" ] && [ -x "$rc" ]; then
             if [ "$RTAGS_SERVER_FILE" ]; then
-                rc -n "$RTAGS_SERVER_FILE" --silent --compile "$i" "$@" &
+                $rc -n "$RTAGS_SERVER_FILE" --silent --compile "$i" "$@" &
             else
-                rc --silent --compile "$i" "$@" &
+                $rc --silent --compile "$i" "$@" &
             fi
         fi
         [ "$RTAGS_RMAKE" ] && exit 0
