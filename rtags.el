9ac9cf4d7a37ac30752d24290af4815788c77da1
Anders Bakken <agbakken@gmail.com>
2013-03-24 21:55:51 -0700

    Use xml, not libxml

--- rtags.el
+++ rtags.el
@@ -9,6 +9,7 @@
 (require 'compile)
 (require 'dabbrev)
 (require 'ido)
+(require 'xml)
 
 (defvar rtags-last-buffer nil)
 (defvar rtags-path-filter nil)
@@ -1060,7 +1061,7 @@ References to references will be treated as references to the referenced symbol"
 (defun rtags-parse-xml-string (xml)
   (with-temp-buffer
     (insert xml)
-    (libxml-parse-xml-region (point-min) (point-max))))
+    (car (xml-parse-region (point-min) (point-max)))))
 
 (defun rtags-parse-overlay-error-node (node filename)
   (when (listp node)
