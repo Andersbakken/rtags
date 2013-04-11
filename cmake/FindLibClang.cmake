ec31ba474b23937eb27de10a9d531876ce9925be
Anders Bakken <agbakken@gmail.com>
2013-04-10 20:05:33 -0700

    Don't let system path (/usr override CLANG_ROOT if set).

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -8,7 +8,10 @@ find_path(CLANG_INCLUDE clang-c/Index.h
           /opt/local/libexec/llvm-3.3/include
           /opt/local/libexec/llvm-3.2/include
           /opt/local/libexec/llvm-3.1/include
-          /opt/local/libexec/llvm-3.0/include)
+          /opt/local/libexec/llvm-3.0/include
+          /usr/local/include
+          /usr/include
+          NO_DEFAULT_PATH)
 
 if (EXISTS ${CLANG_INCLUDE})
   if ("${CLANG_ROOT}" STREQUAL "")
