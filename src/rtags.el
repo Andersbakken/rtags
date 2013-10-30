76735e1e9b459e09e81935492a6ae83b49dadf5e
Anders Bakken <agbakken@gmail.com>
2013-10-30 01:56:48 -0700

    Don't pass context if it's a c++ keyword.

--- src/rtags.el
+++ src/rtags.el
@@ -201,6 +201,26 @@
         (if (keywordp head) (rtags-remove-keyword-params (cdr tail))
           (cons head (rtags-remove-keyword-params tail))))))
 
+(defvar rtags-c++-keywords (make-hash-table :test 'equal))
+(clrhash rtags-c++-keywords)
+(let ((keywords '("alignas" "alignof" "and" "and_eq" "asm" "auto" "bitand"
+                  "bitor" "bool" "break" "case" "catch" "char" "char16_t" "char32_t"
+                  "class" "compl" "const" "constexpr" "const_cast" "continue"
+                  "decltype" "default" "delete" "do" "double" "dynamic_cast" "else"
+                  "enum" "explicit" "export" "extern" "false" "float" "for" "friend"
+                  "goto" "if" "inline" "int" "long" "mutable" "namespace" "new" "noexcept"
+                  "not" "not_eq" "nullptr" "operator" "or" "or_eq" "private" "protected"
+                  "public" "register" "reinterpret_cast" "return" "short" "signed" "sizeof"
+                  "static" "static_assert" "static_cast" "struct" "switch" "template" "this"
+                  "thread_local" "throw" "true" "try" "typedef" "typeid" "typename" "union"
+                  "unsigned" "using" "virtual" "void" "volatile" "wchar_t" "while" "xor" "xor_eq")))
+  (while keywords
+    (puthash (car keywords) t rtags-c++-keywords)
+    (setq keywords (cdr keywords))))
+
+(defun rtags-is-c++-keyword (word)
+  (gethash word rtags-c++-keywords))
+
 (defun* rtags-call-rc (&rest arguments
                              &key (path (buffer-file-name))
                              unsaved
@@ -244,7 +264,7 @@
                 (path nil)
                 (default-directory (push (concat "--with-project=" default-directory) arguments))
                 (t nil))
-          (if context
+          (if (and context (not (rtags-is-c++-keyword context)))
               (push (concat "--context=" context) arguments))
 
           (rtags-log (concat rc " " (combine-and-quote-strings arguments)))
