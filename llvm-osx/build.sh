9f482f58661bac285a55c643f01c930dd4ca1a72
Anders Bakken <agbakken@gmail.com>
2014-02-18 23:19:25 -0800

    Disable assertions.

--- llvm-osx/build.sh
+++ llvm-osx/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-brew install $DIR/llvm.rb --with-clang --with-libcxx
+brew install $DIR/llvm.rb --with-clang --with-libcxx --disable-assertions
