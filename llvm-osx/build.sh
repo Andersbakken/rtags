8b3ee8af497f291d2cfbf85de7860a069edd9ced
Anders Bakken <agbakken@gmail.com>
2014-06-03 10:55:32 -0700

    Allow passing --verbose (and other things to build.sh)

--- llvm-osx/build.sh
+++ llvm-osx/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-brew install $DIR/llvm.rb --with-clang --with-libcxx --disable-assertions
+brew install $DIR/llvm.rb --with-clang --with-libcxx --disable-assertions $@
