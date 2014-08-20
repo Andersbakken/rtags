72bed3ca4050affe4b6bb6125c5846c0481f4e25
Anders Bakken <agbakken@gmail.com>
2014-08-20 00:32:36 -0700

    Remove unused function.

--- src/rtags.el
+++ src/rtags.el
@@ -238,26 +238,6 @@
         (if (keywordp head) (rtags-remove-keyword-params (cdr tail))
           (cons head (rtags-remove-keyword-params tail))))))
 
-(defvar rtags-c++-keywords (make-hash-table :test 'equal))
-(clrhash rtags-c++-keywords)
-(let ((keywords '("alignas" "alignof" "and" "and_eq" "asm" "auto" "bitand"
-                  "bitor" "bool" "break" "case" "catch" "char" "char16_t" "char32_t"
-                  "class" "compl" "const" "constexpr" "const_cast" "continue"
-                  "decltype" "default" "delete" "do" "double" "dynamic_cast" "else"
-                  "enum" "explicit" "export" "extern" "false" "float" "for" "friend"
-                  "goto" "if" "inline" "int" "long" "mutable" "namespace" "new" "noexcept"
-                  "not" "not_eq" "nullptr" "operator" "or" "or_eq" "private" "protected"
-                  "public" "register" "reinterpret_cast" "return" "short" "signed" "sizeof"
-                  "static" "static_assert" "static_cast" "struct" "switch" "template" "this"
-                  "thread_local" "throw" "true" "try" "typedef" "typeid" "typename" "union"
-                  "unsigned" "using" "virtual" "void" "volatile" "wchar_t" "while" "xor" "xor_eq")))
-  (while keywords
-    (puthash (car keywords) t rtags-c++-keywords)
-    (setq keywords (cdr keywords))))
-
-(defun rtags-is-c++-keyword (word)
-  (gethash word rtags-c++-keywords))
-
 (defun* rtags-call-rc (&rest arguments
                              &key (path (buffer-file-name))
                              unsaved
