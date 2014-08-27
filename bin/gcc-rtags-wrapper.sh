33b3a8915524a3ff9c548940b7c3171c46672a87
Anders Bakken <agbakken@gmail.com>
2014-08-27 01:44:16 -0700

    More detection of compiler wrappers.

--- bin/gcc-rtags-wrapper.sh
+++ bin/gcc-rtags-wrapper.sh
@@ -5,6 +5,10 @@ function followSymlink()
     python -c "import os, sys; print(os.path.realpath(\"$1\"))"
 }
 
+if [ "RTAGS_GCC_WRAPPER" = "1" ]; then
+    echo 1>&2 "Recursive invocation of gcc-rtags-wrapper.sh detected"
+    exit 1
+fi
 rc=`which rc`
 for i in `which -a "\`basename $0\`"`; do
     resolved=`followSymlink $i`
@@ -19,6 +23,8 @@ for i in `which -a "\`basename $0\`"`; do
             disown &>/dev/null # rc might be finished by now and if so disown will yell at us
         fi
         [ "$RTAGS_RMAKE" ] && exit 0
+
+        export RTAGS_GCC_WRAPPER=1
         "$i" "$@"
         exit $?
     else
