693fcc5ea701e69bfbf5c25cdd23c25ed6866d24
Aidan Gauland <aidalgol@amuri.net>
2014-11-08 21:09:36 +1300

    Use readlink instead of a Python one-liner
    Use readlink instead of python -c "import os, sys; print(os.path.realpath(\"$1\"))".
    This removes an unnecessary dependency upon Python as well as an
    injection-attack vector: followSymlink can only be used for data that is
    trusted absolutely, because a value of '"; ... "' for $1 (the argument
    to followSymlink), where '...' is arbitrary Python code, will cause the
    input to be executed.

--- bin/gcc-rtags-wrapper.sh
+++ bin/gcc-rtags-wrapper.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
-function followSymlink()
-{
-    python -c "import os, sys; print(os.path.realpath(\"$1\"))"
-}
-
 if [ "RTAGS_GCC_WRAPPER" = "1" ]; then
     echo 1>&2 "Recursive invocation of gcc-rtags-wrapper.sh detected"
     exit 1
 fi
 rc=`which rc`
 for i in `which -a "\`basename $0\`"`; do
-    resolved=`followSymlink $i`
-    filename=`basename $resolved`
+    filename=$(basename $(readlink "$i"))
     if [ "$filename" != "gcc-rtags-wrapper.sh" ] && [ -z "$PLAST" -o "$filename" != "plastc" ]; then
         [ -n "$RTAGS_SERVER_FILE" ] && RTAGS_ARGS="$RTAGS_ARGS -n$RTAGS_SERVER_FILE"
         [ -n "$RTAGS_PROJECT" ] && RTAGS_ARGS="$RTAGS_ARGS --project-root=$RTAGS_PROJECT"
