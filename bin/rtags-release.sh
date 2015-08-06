a2b3256f3178db7052adf90ad8dccc3499dec210
Anders Bakken <agbakken@gmail.com>
2015-08-06 11:58:08 -0700

    Some more work on rtags-release.sh

--- bin/rtags-release.sh
+++ bin/rtags-release.sh
@@ -11,22 +11,18 @@ SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" || branch_name="(unnamed branch)"     # detached HEAD
 branch_name=${branch_name##refs/heads/}
 
+RELEASES=~/Downloads/rtags-releases
 if [ "$branch_name" == "master" ]; then
     commit=`git show --oneline --no-patch`
-    cd "$SCRIPT_PATH/.."
-    rm -f rtags.tar
-    $SCRIPT_PATH/git-archive-all rtags.tar
-    rm -rf rtags-release-repo
-    git new-workdir $PWD rtags-release-repo gh-pages
-    cd rtags-release-repo
-    git pull origin gh-pages
-    rm -f rtags.tar.gz rtags.tar.bz2 rtags.tar
-    mv ../rtags.tar .
+    rm -rf $RELEASES
+    mkdir $RELEASES
+    $SCRIPT_PATH/git-archive-all $RELEASES/rtags.tar
+    cd $RELEASES
+    git init
     gzip --keep rtags.tar
     bzip2 rtags.tar
     git add rtags.tar.gz rtags.tar.bz2
     git commit -m "Release for $commit"
-    git push origin gh-pages
-    cd ..
-    rm -rf rtags-release-repo
+    git push -f git@github.com:Andersbakken/rtags-releases.git master
+    # rm -rf $RELEASES
 fi
