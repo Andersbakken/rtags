340d94846cae60d994859921a8465ce7a177f3c7
Yutian Li <hotpxless@gmail.com>
2015-08-13 01:28:46 +0800

    [master] Add dependency

--- src/rtags.el
+++ src/rtags.el
@@ -5,6 +5,7 @@
 ;; Author: Jan Erik Hanssen <jhanssen@gmail.com>
 ;;         Anders Bakken <agbakken@gmail.com>
 ;; URL: http://rtags.net
+;; Package-Requires: ((popup "0.5.3"))
 
 ;; This file is not part of GNU Emacs.
 
