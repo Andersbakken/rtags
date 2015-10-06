a57c80c6ecd0ab2a5850efe77856f9ff3cd69fa7
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2015-10-06 12:50:25 +0200

    Simplify rtags bash-completion file.
    There is no real speed improvement for caching, so its safe to remove
    it.
    Added Cygewin support

--- src/rtags-bash-completion.bash
+++ src/rtags-bash-completion.bash
@@ -1,29 +1,27 @@
-complete-rtags ()
+_rtags ()
 {
-    app=${COMP_WORDS[0]}
-    test -x "$app" || app=$(which $app)
-    modified="$(ls -la \"$app\" | awk '{print $5,$6,$7,$8}')"
-    appname="$(basename $app)"
+    local prev value_opts non_value_opts
 
-    cache="/tmp/completions-$appname-$(id -u)"
-    if [ ! -e "$cache" ] || [ "$modified" != "$(head -n 1 $cache)" ]; then
-        echo $modified > "$cache"
-        "$app" --help | grep '^ \+-' | grep "\[[A-Za-z]*\]" | sed -e 's,\([^ ]\) .*,\1,' -e 's,|, ,' | xargs >> "$cache"
-        "$app" --help | grep '^ \+-' | grep -v "\[[A-Za-z]*\]" | sed -e 's,\([^ ]\) .*,\1,' -e 's,|, ,' | xargs >> "$cache"
-    fi
-    local valueopts=$(head -n 2 "$cache" | tail -n 1)
-    local prev=${COMP_WORDS[COMP_CWORD-1]}
-    if [ -n "$prev" ] && printf -- "${valueopts}\n" | grep --quiet -- "$prev"; then
+    app=${COMP_WORDS[0]}
+    value_opts=$($app --help | grep '^ \+-' | grep "\[[A-Za-z]*\]" | sed -e 's,\([^ ]\) .*,\1,' -e 's,|, ,')
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+
+    if [ -n "$prev" ] && printf -- "${value_opts}\n" | grep --quiet -- "$prev"; then
         COMPREPLY=()
         return;
     fi
 
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    local nonvalueopts=$(tail -n 1 $cache)
-    COMPREPLY=($(compgen -W "$valueopts $nonvalueopts" -- $cur))
+    cur=${COMP_WORDS[COMP_CWORD]}
+    non_value_opts=$($app --help | grep '^ \+-' | grep -v "\[[A-Za-z]*\]" | sed -e 's,\([^ ]\) .*,\1,' -e 's,|, ,')
+
+    COMPREPLY=($(compgen -W "$value_opts $non_value_opts" -- $cur))
+
     if [ -n "$cur" ] && [ ${#COMPREPLY[@]} -eq 0 ] && printf -- "$cur\n" | grep --quiet -- "^-[^-]"; then
-        COMPREPLY=($(compgen -W "$valueopts $nonvalueopts" -- -$cur))
+        COMPREPLY=($(compgen -W "$value_opts $non_value_opts" -- -$cur))
     fi
 }
+complete -F _rtags -o default rc rdm
 
-complete -F complete-rtags -o default rc rdm ./rc ./rdm
+if [ Cygwin = "$(uname -o 2>/dev/null)" ]; then
+    complete -F _rtags -o default rc.exe rdm.exe
+fi
