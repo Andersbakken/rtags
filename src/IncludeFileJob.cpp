dcc5581dfbca62f018a90462984d06149f8b74ae
Anders Bakken <agbakken@gmail.com>
2015-10-08 22:45:33 -0700

    Some support for forwarding headers.

--- src/IncludeFileJob.cpp
+++ src/IncludeFileJob.cpp
@@ -33,64 +33,93 @@ IncludeFileJob::IncludeFileJob(const std::shared_ptr<QueryMessage> &query, const
     mSymbol = query->query();
 }
 
+static inline List<Path> headersForSymbol(const std::shared_ptr<Project> &project, const Location &loc)
+{
+    List<Path> ret;
+    const Path &path = loc.path();
+    if (path.isHeader()) {
+        ret.append(path);
+        if (const DependencyNode *node = project->dependencies().value(loc.fileId())) {
+            for (const auto &dependent : node->dependents) {
+                const Path p = Location::path(dependent.first);
+                if (p.isHeader() && dependent.second->includes.size() == 1) {
+                    ret.append(p);
+                    // allow headers that only include one header if we don't
+                    // find anything for the real header
+                }
+            }
+        }
+    }
+    return ret;
+}
+
 int IncludeFileJob::execute()
 {
-    if (mSource.isNull())
+    if (mSource.isNull()) {
         return 1;
+    }
     const Path directory = mSource.sourceFile().parentDir();
     const bool fromHeader = queryMessage()->currentFile().isHeader();
     Set<Location> last;
     int matches = 0;
     auto process = [&directory, &fromHeader, this](const Set<Location> &locations) {
         for (const Location &loc : locations) {
-            const Path path = loc.path();
-            if (!path.isHeader())
-                continue;
-            const Symbol sym = project()->findSymbol(loc);
-            if (sym.isDefinition()) {
-                switch (sym.kind) {
-                case CXCursor_FunctionDecl:
-                case CXCursor_FunctionTemplate:
-                case CXCursor_ClassDecl:
-                case CXCursor_StructDecl:
-                case CXCursor_ClassTemplate: {
-                    List<String> alternatives;
-                    if (path.startsWith(directory))
-                        alternatives << String::format<256>("#include \"%s\"", path.mid(directory.size()).constData());
-                    for (const Source::Include &inc : mSource.includePaths) {
-                        const Path p = inc.path.ensureTrailingSlash();
-                        if (path.startsWith(p)) {
-                            alternatives << String::format<256>("#include <%s>", path.mid(p.size()).constData());
-                        }
-                    }
-                    const int tail = strlen(path.fileName()) + 1;
-                    List<String>::iterator it = alternatives.begin();
-                    while (it != alternatives.end()) {
-                        bool drop = false;
-                        for (List<String>::const_iterator it2 = it + 1; it2 != alternatives.end(); ++it2) {
-                            if (it2->size() < it->size()) {
-                                if (!strncmp(it2->constData() + 9, it->constData() + 9, it2->size() - tail - 9)) {
-                                    drop = true;
-                                    break;
-                                }
+            bool first = true;
+            for (const Path &path : headersForSymbol(project(), loc)) {
+                bool found = false;
+                const Symbol sym = project()->findSymbol(loc);
+                if (sym.isDefinition()) {
+                    switch (sym.kind) {
+                    case CXCursor_FunctionDecl:
+                    case CXCursor_FunctionTemplate:
+                    case CXCursor_ClassDecl:
+                    case CXCursor_StructDecl:
+                    case CXCursor_ClassTemplate: {
+                        List<String> alternatives;
+                        if (path.startsWith(directory))
+                            alternatives << String::format<256>("#include \"%s\"", path.mid(directory.size()).constData());
+                        for (const Source::Include &inc : mSource.includePaths) {
+                            const Path p = inc.path.ensureTrailingSlash();
+                            if (path.startsWith(p)) {
+                                alternatives << String::format<256>("#include <%s>", path.mid(p.size()).constData());
                             }
                         }
-                        if (drop) {
-                            it = alternatives.erase(it);
-                        } else {
-                            ++it;
+                        const int tail = strlen(path.fileName()) + 1;
+                        List<String>::iterator it = alternatives.begin();
+                        while (it != alternatives.end()) {
+                            bool drop = false;
+                            for (List<String>::const_iterator it2 = it + 1; it2 != alternatives.end(); ++it2) {
+                                if (it2->size() < it->size()) {
+                                    if (!strncmp(it2->constData() + 9, it->constData() + 9, it2->size() - tail - 9)) {
+                                        drop = true;
+                                        break;
+                                    }
+                                }
+                            }
+                            if (drop) {
+                                it = alternatives.erase(it);
+                            } else {
+                                ++it;
+                            }
                         }
+                        std::sort(alternatives.begin(), alternatives.end(), [](const String &a, const String &b) {
+                                return a.size() < b.size();
+                            });
+                        for (const auto &a : alternatives) {
+                            found = true;
+                            write(a);
+                        }
+                        break; }
+                    default:
+                        break;
                     }
-                    std::sort(alternatives.begin(), alternatives.end(), [](const String &a, const String &b) {
-                            return a.size() < b.size();
-                        });
-                    for (const auto &a : alternatives) {
-                        write(a);
-                    }
-                    break; }
-                default:
-                    break;
                 }
+                if (first) {
+                    if (found)
+                        break;
+                    first = false;
+                }
+
             }
         }
     };
