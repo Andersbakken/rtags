73f4da6bade1f86a87745f589ae05bb5e1b3ce0c
Anders Bakken <agbakken@gmail.com>
2015-10-13 13:52:40 -0700

    Don't consider non-file-buffers that (except dired)

--- src/rtags.el
+++ src/rtags.el
@@ -2630,7 +2630,8 @@ is true. References to references will be treated as references to the reference
 ;;;###autoload
 (defun rtags-update-current-project ()
   (interactive)
-  (when (and (not (eq (current-buffer) rtags-last-update-current-project-buffer))
+  (when (and (or (buffer-file-name) dired-directory)
+             (not (eq (current-buffer) rtags-last-update-current-project-buffer))
              default-directory
              (not (rtags-is-tramp))
              (file-directory-p default-directory))
