1526f7c4ba6462f2c277d1bf1d8de12ed7d0c41d
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2015-10-14 20:00:06 +0200

    Cache LIBCLANG_LIBDIR, and fail if it is empty.
    - Cache to LIBCLANG_LIBDIR variable so the user can adjust it with
      ccmake or in the CMakeCache.txt file also mark it as advanced.
    
    - We should fail at build time if the variable libclang is not defined
      this makes the `#if not defined CLANG_LIBDIR redundant, which didn't
      fail anyway, since the define `-DCLANG_LIBDIR` always gets defined but
      is just empty.

--- cmake/FindLibClang.cmake
+++ cmake/FindLibClang.cmake
@@ -10,7 +10,7 @@
 #   the llvm-config tool to get various information.
 # ``LIBCLANG_LIBRARIES``
 #   the clang libraries to link against to use Clang/LLVM.
-# ``LIBCLANG_LIBRARY_DIRS``
+# ``LIBCLANG_LIBDIR``
 #   the directory where the clang libraries are located.
 # ``LIBCLANG_FOUND``
 #   true if libclang was found
@@ -82,6 +82,7 @@ if (NOT EXISTS ${LIBCLANG_LIBDIR})
   if (NOT EXISTS ${LIBCLANG_LIBDIR})
     message(FATAL_ERROR "Could NOT find clang libdir. You can fix this by setting LIBCLANG_LIBDIR in your shell or as a cmake variable.")
   endif ()
+  set(LIBCLANG_LIBDIR ${LIBCLANG_LIBDIR} CACHE STRING "Path to the clang library.")
 endif ()
 
 if (NOT LIBCLANG_LIBRARIES)
@@ -104,4 +105,4 @@ message("-- Using Clang version ${LIBCLANG_VERSION_STRING} from ${LIBCLANG_LIBDI
 # Handly the QUIETLY and REQUIRED arguments and set LIBCLANG_FOUND to TRUE if all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibClang DEFAULT_MSG LIBCLANG_LIBRARY LIBCLANG_CXXFLAGS LIBCLANG_LIBDIR)
-mark_as_advanced(LIBCLANG_CXXFLAGS LIBCLANG_LIBRARY LIBCLANG_LLVM_CONFIG_EXECUTABLE)
+mark_as_advanced(LIBCLANG_CXXFLAGS LIBCLANG_LIBRARY LIBCLANG_LLVM_CONFIG_EXECUTABLE LIBCLANG_LIBDIR)
