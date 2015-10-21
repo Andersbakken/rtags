613d22c7c2e3b713b6d13a5b125cb5f8a945005b
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2015-10-21 21:15:46 +0200

    Use pkg-config to figure out if bash completion support is available
    - If PKG was not found we fail silently
    - If PKG could not find bash-completion support, we fail silently, since
      bash completion is not a must
    - Use `/usr` as default install path if the user has not provided a path
      as argument, this is the default install location.
    
    This is the best option to install the bash completion files, right now.
    We should mention in the README, if the user wants bash completion
    support he/she needs to make sure that the `pkg-config` tool and the
    `bash-completion` package is installed. Also this makes it easier, in
    case rtags gets packaged as `rpm` or `deb` file one they or another.

---
+++ cmake/BashCompletion.cmake
@@ -0,0 +1,17 @@
+find_package(PkgConfig QUIET)
+if(PKG_CONFIG_FOUND)
+    execute_process(COMMAND ${PKG_CONFIG_EXECUTABLE} --variable=completionsdir bash-completion
+        RESULT_VARIABLE UNAVAILABLE
+        OUTPUT_VARIABLE COMPLETIONS_DIR
+        ERROR_QUIET
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if(NOT UNAVAILABLE)
+        if(${CMAKE_INSTALL_PREFIX} MATCHES "^/usr")
+            string(REGEX REPLACE "^/usr" "" COMPLETIONS_DIR ${COMPLETIONS_DIR})
+        endif()
+        set(BASH_COMPLETION_COMPLETIONSDIR "${CMAKE_INSTALL_PREFIX}${COMPLETIONS_DIR}"
+            CACHE PATH "Bash completion installation directory" FORCE)
+        mark_as_advanced(BASH_COMPLETION_COMPLETIONSDIR)
+        set(BASH_COMPLETION_FOUND TRUE)
+    endif()
+endif()
