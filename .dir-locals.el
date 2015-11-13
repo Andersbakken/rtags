86b880691a2ed7b2b1196942ce9bd7b7ec6cce1f
Anders Bakken <agbakken@gmail.com>
2015-11-12 21:55:09 -0800

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c++-mode . ((c-basic-offset . 2)
+              (indent-tabs-mode . nil))))
+((cmake-mode . ((cmake-tab-width . 4)
+                (indent-tabs-mode . nil))))
+((nil . ((indent-tabs-mode . nil))))
+
