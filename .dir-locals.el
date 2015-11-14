7c434124fe2659e9db1f515fcc2a33ebd22353b2
Anders Bakken <agbakken@gmail.com>
2015-11-14 10:20:44 -0800

    Oops. 4 indent.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,7 +1,7 @@
 ;;; Directory Local Variables
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
-((c++-mode . ((c-basic-offset . 2)
+((c++-mode . ((c-basic-offset . 4)
               (indent-tabs-mode . nil))))
 ((cmake-mode . ((cmake-tab-width . 4)
                 (indent-tabs-mode . nil))))
