27858b6b7bcb1cff986f1cecfde1c64e2aa24d1b
Atila Neves <aalvesne@cisco.com>
2016-01-04 10:18:24 +0100

    Fix rtags-ac bug

--- src/rtags-ac.el
+++ src/rtags-ac.el
@@ -13,7 +13,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with RTags.  If not, see <http://www.gnu.org/licenses/>.
 
-(eval-when-compile (require' cl))
+(eval-when-compile (require 'cl))
 (require 'auto-complete)
 (require 'rtags)
 
