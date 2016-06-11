e26325464fb118ee9e8c1092d0b8f73e2b456ff0
Anders Bakken <agbakken@gmail.com>
2016-06-11 13:16:52 -0700

    Bump to clang 3.8.

--- scripts/llvm-osx/build.sh
+++ scripts/llvm-osx/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 brew tap homebrew/versions
-brew install llvm36 --with-libcxx --with-clang --disable-assertions --rtti $@
+brew install llvm38 --with-libcxx --with-clang --disable-assertions --rtti $@
