5cc33b9f9cf8c7719b9c94139fd1c9d07e6c6585
Anders Bakken <agbakken@gmail.com>
2016-07-08 13:58:21 -0700

    Fix the brew argument. It seems that it should be --without-assertions

--- scripts/llvm-osx/build.sh
+++ scripts/llvm-osx/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 brew tap homebrew/versions
-brew install llvm38 --with-libcxx --with-clang --disable-assertions --rtti $@
+brew install llvm38 --with-libcxx --with-clang --without-assertions --rtti $@
