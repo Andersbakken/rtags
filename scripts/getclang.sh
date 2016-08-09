a9c389666efe20b84d0960cf3e1537f3a972d5bc
Anders Bakken <agbakken@gmail.com>
2016-08-08 20:15:39 -0700

    Add some verification since these tarballs for some reason come off http.
    There are .sig files that could be used to verify the contents with gpg
    but I wasn't quite able to make it work so I figured I'd rather check in
    known sha512 checksums.

--- scripts/getclang.sh
+++ scripts/getclang.sh
@@ -3,6 +3,8 @@
 VERSION="$1"
 [ -z "${VERSION}" ] && VERSION="3.8.1"
 
+CHECKSUMDIR=`dirname "${BASH_SOURCE[0]}"`/../llvm-checksums/
+
 LLVM=llvm-${VERSION}.src
 LLVM_FILE=${LLVM}.tar.xz
 CLANG=cfe-${VERSION}.src
@@ -10,18 +12,34 @@ CLANG_FILE=${CLANG}.tar.xz
 LIBCXX=libcxx-${VERSION}.src
 LIBCXX_FILE=${LIBCXX}.tar.xz
 
-STATUS=`curl -w "%{http_code}" -z ${LLVM_FILE} http://llvm.org/releases/${VERSION}/${LLVM_FILE} -o ${LLVM_FILE}`
-if [ ${STATUS} == 200 ] || [ ! -d llvm ]; then
+function dnload()
+{
+    local FILE=$1-${VERSION}.src.tar.xz
+    local URL=http://llvm.org/releases/${VERSION}/${FILE}
+    STATUS=`curl -w "%{http_code}" -z ${FILE} http://llvm.org/releases/${VERSION}/${FILE} -o ${FILE} 2>/dev/null`
+    if [ "${STATUS}" == 200 ] || [ "${STATUS}" == 304 ]; then
+        local CHECKSUM=`cat "${CHECKSUMDIR}/${FILE}.sha512"`
+        local ACTUAL=`sha512sum $FILE | awk '{print $1}'`
+        if [ "${CHECKSUM}" != "$ACTUAL" ]; then
+            echo -e "Invalid checksum, expected:\n${CHECKSUM}\nGot:\n${ACTUAL}\n" >&2
+            exit 1
+        fi
+    fi
+    echo ${STATUS}
+}
+
+STATUS=`dnload llvm`
+if [ "${STATUS}" == 200 ] || [ ! -d llvm ]; then
     tar xf ${LLVM_FILE}
     rm -rf llvm
     mv ${LLVM} llvm
-    curl -z ${CLANG_FILE} http://llvm.org/releases/${VERSION}/${CLANG_FILE} -o ${CLANG_FILE}
+    dnload cfe >/dev/null
     tar xf ${CLANG_FILE}
     rm -f llvm/tools/clang
     mv ${CLANG} llvm/tools/clang
 
     if [ `uname -s` = Darwin ]; then
-        curl -z ${LIBCXX_FILE} http://llvm.org/releases/${VERSION}/${LIBCXX_FILE} -o ${LIBCXX_FILE}
+        dnload libcxx >/dev/null
         tar xf ${LIBCXX_FILE}
         mv ${LIBCXX} llvm/projects/libcxx
     fi
