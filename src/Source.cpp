Andersbakken/rtags

commit 6641603502e6b94a5e9da7207f1e473c1c0080b0

Properly parse -ofoo.o as well as -o foo.o

src/Source.cpp
@@ -627,10 +627,16 @@ List<Source> Source::parse(const String &cmdLine,
                     root.resolve();
                     arguments.append(root);
                 }
-            } else if (arg == "-o") {
-                if (i + 1 < s) {
+            } else if (arg.startsWith("-o")) {
+                Path p;
+                if (arg.size() > 2) {
+                    p = arg.mid(2);
+                } else if (i + 1 < s) {
+                    p = split.value(++i);
+                }
+                if (!p.isEmpty()) {
                     bool ok;
-                    Path p = Path::resolved(split.value(++i), Path::RealPath, path, &ok);
+                    p = Path::resolved(split.value(++i), Path::RealPath, path, &ok);
                     // error() << p << ok << split.value(i) << Path::resolved(split.value(i), Path::MakeAbsolute);
                     if (!ok && !p.isAbsolute()) {
                         p.prepend(path); // the object file might not exist

commit 251784242f4199b10b720e31ebb9178b93a9278a

Fix wrong -o argument parsing
Regression came from 6641603502e6b94a5e9da7207f1e473c1c0080b0 revision

src/Source.cpp
@@ -636,7 +636,7 @@ List<Source> Source::parse(const String &cmdLine,
                 }
                 if (!p.isEmpty()) {
                     bool ok;
-                    p = Path::resolved(split.value(++i), Path::RealPath, path, &ok);
+                    p = Path::resolved(p, Path::RealPath, path, &ok);
                     // error() << p << ok << split.value(i) << Path::resolved(split.value(i), Path::MakeAbsolute);
                     if (!ok && !p.isAbsolute()) {
                         p.prepend(path); // the object file might not exist