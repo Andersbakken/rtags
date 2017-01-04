013f04cc45ce3f745c20b7741293992668e457be
Anders Bakken <agbakken@gmail.com>
2017-01-04 12:19:09 -0800

    Compile.

--- src/AST.cpp
+++ src/AST.cpp
@@ -116,7 +116,7 @@ static void registerClasses(sel::State &state)
 
 std::shared_ptr<AST> AST::create(const Source &source, const String &sourceCode, CXTranslationUnit unit)
 {
-    auto ast = std::make_shared<AST>();
+    std::shared_ptr<AST> ast(new AST);
     ast->mState.reset(new sel::State {true});
     sel::State &state = *ast->mState;
     registerClasses(state);
