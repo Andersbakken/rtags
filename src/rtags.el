Andersbakken/rtags

commit 44516d183ea960c32172b40f9df85d5a74b28627

Partial fix for #936.
This isn't really a proper fix but the code is a bit of mess so it
would take some work to do it properly.

Display only filename in references tree.

src/rtags.el
@@ -1687,10 +1687,11 @@ instead of file from `current-buffer'.
 (defun rtags-references-tree-current-location ()
   (save-excursion
     (goto-char (point-at-bol))
-    (and (looking-at "\\( *\\)\\([^ ]+:[0-9]+:[0-9]+:\\)")
-         (cons
-          (concat rtags-current-project (buffer-substring-no-properties (match-beginning 2) (match-end 2)))
-          (/ (length (match-string 1)) rtags-tree-indent)))))
+    (skip-chars-forward " ")
+    (let ((prop (get-text-property (point) 'rtags-ref-location)))
+      (and prop
+           (cons (format "%s:%d:%d:" (car prop) (cadr prop) (caddr prop))
+                 (/ (- (point) (point-at-bol)) rtags-tree-indent))))))
 
 (defun rtags-references-tree-collapse-all ()
   (interactive)
@@ -1860,9 +1861,10 @@ instead of file from `current-buffer'.
                     (when (rtags-bookmark-set (format "RTags_%d" rtags-buffer-bookmarks))
                       (incf rtags-buffer-bookmarks)
                       (1- rtags-buffer-bookmarks)))))))))
-    (insert (rtags-tree-indent level) location " " (rtags-format-context (cdr (assoc 'ctx ref)) .4))
+    (insert (rtags-tree-indent level) (file-name-nondirectory location) " " (rtags-format-context (cdr (assoc 'ctx ref)) .4))
     (let ((cf (cdr (assoc 'cf ref)))
-          (props (list 'rtags-ref-containing-function-location (cdr (assoc 'cfl ref))))
+          (props (list 'rtags-ref-containing-function-location (cdr (assoc 'cfl ref))
+                       'rtags-ref-location components))
           (pos (point)))
       (when bookmark-idx
         (setq props (append props (list 'rtags-bookmark-index (cons bookmark-idx (point-at-bol))))))

commit e98a5e2de5a3308228e437a424259e2e03505ee4

Fix breakage introduced by 44516d183ea960c32172b40f9df85d5a74b28627.

src/rtags.el
@@ -3639,11 +3639,14 @@ other window instead of the current one."
           (t
            (when (cdr idx)
              (goto-char (cdr idx)))
-           (rtags-goto-location (buffer-substring-no-properties (save-excursion
-                                                                  (goto-char (point-at-bol))
-                                                                  (skip-chars-forward " ")
-                                                                  (point))
-                                                                (point-at-eol)) nil other-window)
+           (let ((refloc (car (rtags-references-tree-current-location))))
+             (if refloc
+                 (rtags-goto-location refloc nil other-window)
+               (rtags-goto-location (buffer-substring-no-properties (save-excursion
+                                                                      (goto-char (point-at-bol))
+                                                                      (skip-chars-forward " ")
+                                                                      (point))
+                                                                    (point-at-eol)) nil other-window)))
            (when bookmark
              (bookmark-set bookmark))))
     (if remove