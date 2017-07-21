d1345d54ffab7129013f73826140f9eecd4b6281
Anders Bakken <agbakken@gmail.com>
2017-07-21 09:06:45 -0700

    Issue #1003. Add a script to bump version for rtags release.

---
+++ scripts/bump-version.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+SCRIPT="$(readlink -f ${BASH_SOURCE[0]})"
+DIR="$(dirname $SCRIPT)"
+
+# echo "$SCRIPT $DIR"
+
+MAJOR=`echo $1 | awk -F. '{print $1}'`
+MINOR=`echo $1 | awk -F. '{print $2}'`
+
+if ! echo "$1" | grep -q "^[0-9]\+\.[0-9]\+$"; then
+    echo "Bad argument: \"$1\""
+    echo "Usage bump-version.sh 3.22"
+    exit 1
+fi
+
+sed -i"" -e "s,^set(RTAGS_VERSION_MAJOR [0-9]\+),set(RTAGS_VERSION_MAJOR $MAJOR)," "$DIR/../CMakeLists.txt" -e "s,^set(RTAGS_VERSION_MINOR [0-9]\+),set(RTAGS_VERSION_MINOR $MINOR)," "$DIR/../CMakeLists.txt"
+sed -i"" -e "s,^(defconst rtags-package-version \"[0-9]\+\.[0-9]\+\"),(defconst rtags-package-version \"${MAJOR}.${MINOR}\")," "$DIR/../src/rtags.el"
+sed -i"" -e "s,https://andersbakken\.github\.io/rtags-releases/rtags-[0-9]\+\.[0-9]\+\.tar\.,https://andersbakken.github.io/rtags-releases/rtags-$MAJOR.$MINOR.tar.,g" "$DIR/../README.org"
