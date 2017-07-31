db1244c1c14514324474f362c857112e89bbf0c6
Anders Bakken <agbakken@gmail.com>
2017-07-31 10:57:37 -0700

    Oops

--- src/rtags.el
+++ src/rtags.el
@@ -68,7 +68,7 @@
 ;; Constants
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defconst rtags-protocol-version 124)
-(defconst rtags-package-version "2.11")
+(defconst rtags-package-version "2.12")
 (defconst rtags-popup-available (require 'popup nil t))
 (defconst rtags-supported-major-modes '(c-mode c++-mode objc-mode) "Major modes RTags supports.")
 (defconst rtags-verbose-results-delimiter "------------------------------------------")
