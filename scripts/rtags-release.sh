59bf2084efd81454e91551666c3f142279507e67
Anders Bakken <agbakken@gmail.com>
2017-09-04 21:48:37 -0700

    Improve release script somewhat

--- scripts/rtags-release.sh
+++ scripts/rtags-release.sh
@@ -1,68 +1,80 @@
 #!/bin/bash
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-    SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    SOURCE="$(readlink "$SOURCE")"
-    [[ $SOURCE != /* ]] && SOURCE="$SCRIPT_PATH/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPT_PATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-REPO=$SCRIPT_PATH/../
+### TO RUN
+### curl https://raw.githubusercontent.com/Andersbakken/rtags/master/scripts/rtags-release.sh | bash
 
-PULL=
-while [ -n "$1" ]; do
+REPO=~/Downloads/rtags-repo
+RELEASES_REPO=~/Downloads/rtags-releases
+CMAKE_ARGS="-DRTAGS_ENABLE_DEV_OPTIONS=1"
+FORCE=
+
+while [ -n "$1" ]; do 
     case "$1" in
-        --pull|-p)
-            PULL=1
-            ;;
         --repo|-r)
             shift
-            REPO=$1
+            REPO="$1"
+            ;;
+        --releases-repo|-R)
+            shift
+            RELEASES_REPO="$1"
+            ;;
+        --force|-f)
+            FORCE=1
+            ;;
+        --cmake-args|-C)
+            shift
+            CMAKE_ARGS="$CMAKE_ARGS $1"
             ;;
         *)
-            echo "Unknown argument $1"
+            echo "Unknown option $1"
             exit 1
             ;;
     esac
     shift
 done
-cd "$REPO"
-UNSTASH=
-if [ "$PULL" ]; then
-    if [ -n "`git status -s -uno 2>/dev/null`" ]; then
-        git stash
-        UNSTASH=1
-    fi
-    git submodule update > /dev/mull
-    if ! git pull --recurse-submodules > /dev/null; then
-        if [ -n "$UNSTASH" ]; then
-            git stash pop
-            exit 1
-        fi
-    fi
+
+if [ ! -d "$REPO" ]; then
+    git clone git@github.com:Andersbakken/rtags.git "$REPO" --recursive || exit 1
+else
+    cd "$REPO"
+    git checkout -f master
+    git pull --autostash || exit 1
 fi
+
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)" || branch_name="(unnamed branch)"     # detached HEAD
 branch_name=${branch_name##refs/heads/}
-
-RELEASES=`mktemp -d`
-if [ "$branch_name" == "master" ]; then
-    commit=$(git show --oneline --no-patch)
-    current=`curl --silent http://andersbakken.github.io/rtags-releases/commit | cut -d' ' -f1`
-    if [ "`echo "$commit" | cut -d' ' -f1`" != "$current" ]; then
-        cd "$RELEASES"
-        cmake "$REPO" -DRTAGS_ENABLE_DEV_OPTIONS=1 >/dev/null 2>&1
-        make package_source >/dev/null
-        cmake "$REPO" -DRTAGS_ENABLE_DEV_OPTIONS=1 -DCPACK_GENERATOR=TBZ2 >/dev/null 2>&1
-        make package_source >/dev/null
-        git init >/dev/null
-        rm -rf .git/hooks/*
-        git checkout -b gh-pages >/dev/null 2>&1
-        echo "$commit" > commit
-        git add *.tar.gz *.tar.bz2 commit >/dev/null
-        git commit -m "Release for $commit" >/dev/null
-        git push -f git@github.com:Andersbakken/rtags-releases.git gh-pages >/dev/null
-        rm -rf "$RELEASES"
-    fi
+if [ "$branch_name" != "master" ]; then
+    echo wrong branch $branch_name
+    exit 1
 fi
 
-[ -n "$UNSTASH" ] && git stash pop
+commit=$(git show --oneline --no-patch)
+current=`curl --silent http://andersbakken.github.io/rtags-releases/commit | cut -d' ' -f1`
+if [ "`echo "$commit" | cut -d' ' -f1`" = "$current" -a -z "$FORCE" ]; then
+    exit 0
+fi
+
+if [ ! -d "$RELEASES_REPO" ]; then
+    git clone git@github.com:Andersbakken/rtags-releases.git "$RELEASES_REPO" --recursive || exit 1
+    cd "$RELEASES_REPO"
+else
+    cd "$RELEASES_REPO"
+    git pull --autostash || exit 1
+fi
+
+if ! git branch | grep --quiet "^\* *gh-pages$"; then
+    echo "wrong branch"
+    exit 1
+fi
+
+rm -rf .git/hooks/*
+
+cmake "$REPO" $CMAKE_ARGS >/dev/null 2>&1
+make package_source >/dev/null
+cmake "$REPO" $CMAKE_ARGS -DCPACK_GENERATOR=TBZ2 >/dev/null 2>&1
+make package_source >/dev/null
+echo "$commit" > commit
+git add *.tar.gz *.tar.bz2 commit >/dev/null
+git commit --amend -m "Release for $commit" >/dev/null
+git push -f git@github.com:Andersbakken/rtags-releases.git gh-pages >/dev/null
+git clean -xfd 
