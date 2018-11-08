56b2cba350f664b31f52a160a295a0db883acee0
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2018-11-08 23:58:36 +0100

    Revert fix for iOS travis build from 37fafe6

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -60,7 +60,6 @@ function osx()
 {
     ## Step -- Setup
     brew update
-    rm /usr/local/include/c++
     brew install llvm yarn cppunit
     brew upgrade python3
     python3 -m pip install --upgrade pip
