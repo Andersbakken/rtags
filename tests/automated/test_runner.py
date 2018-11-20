fd0f56b16081b008f1e23290470209f4c010ac9b
Christian Schwarzgruber <c.schwarzgruber.cs@gmail.com>
2018-11-20 09:29:36 +0100

    [test_runner.py] FileNotFoundError is not available in python 2.7
    Update docstrings.

--- tests/automated/test_runner.py
+++ tests/automated/test_runner.py
@@ -5,7 +5,7 @@
 #
 # Assuming that the RTags binaries are in build/bin, run with:
 #
-# $ RTAGS_BINARY_DIR=$(pwd)/build/bin:$PATH  nosetests --no-byte-compile
+# $ RTAGS_BINARY_DIR=$(pwd)/build/bin  nosetests --no-byte-compile
 #
 # Run nosetest with --nocapture flag to see print output
 #
@@ -22,6 +22,11 @@ import time
 import subprocess as sp
 from hamcrest import assert_that, has_length, has_item, empty, is_not, equal_to
 
+try:
+    FileNotFoundError
+except NameError:
+    FileNotFoundError = IOError
+
 sys.dont_write_bytecode = True
 os.environ["PYTHONDONTWRITEBYTECODE"] = "1"
 SOCKET_FILE = "/var/tmp/rdm_dev"
@@ -35,35 +40,26 @@ except KeyError:
 
 
 def create_compile_commands(test_dir, test_files):
-    """
-    Create dict of compile commands
-    """
+    """Create dict of compile commands."""
     return [dict(directory=os.path.abspath(test_dir), file=test_file,
                  command="clang++ -std=c++11 -I. -c %s" % os.path.join(test_dir, test_file))
             for test_file in (src_file for src_file in test_files
                               if src_file.endswith(('.cpp', '.c')))]
 
 def read_locations(test_dir, lines):
-    """
-    Read location
-    """
+    """Read location."""
     lines = [line.split(":") for line in lines.split("\n") if len(line) > 0]
     return [Location(os.path.join(test_dir, line[0]), line[1], line[2]) for line in lines]
 
 
-class Location:
-    """
-    Class representing location in file
-    """
+class Location(object):
+    """Class representing location in file."""
     def __init__(self, file, line, col):
         self.file, self.line, self.col = str(file), int(line), int(col)
 
-    # pylint: disable=
     @classmethod
     def from_str(cls, string):
-        """
-        From string.
-        """
+        """From string."""
         return cls(*string.split(":")[0:3])
 
     def __eq__(self, other):
@@ -80,18 +76,19 @@ class Location:
 
 
 def run_rc(args):
-    """
-    Run rc command.
-    """
+    """Run rc command."""
     args = [RC, "--socket-file=" + SOCKET_FILE] + args
     return sp.check_output(args).decode()
 
-class TestType:
+# pylint: disable=too-few-public-methods
+class TestType(object):
+    """Enum class representing the test types we support."""
     location = 1
     parse = 2
     completion = 3
 
 def run_location(test_dir, rc_command, expected):
+    """Run location test, and compare with EXPECTED output."""
     actual_locations = read_locations(test_dir,
                                       run_rc([c.format(test_dir) for c in rc_command]))
     # Compare that we have the same results in length and content
@@ -101,27 +98,28 @@ def run_location(test_dir, rc_command, expected):
         assert_that(actual_locations, has_item(expected_location))
 
 def run_parse(test_dir, rc_command, expected):
+    """Run a parse test, and compare with EXPECTED output."""
     output = run_rc(rc_command)
     assert_that(output, is_not(empty()))
     assert_that(output.split(" "), has_item(expected.format(test_dir + "/")))
 
-def non_empty_split(s, delim=None):
-    return [x for x in s.split(delim) if x]
+def non_empty_split(string, delim=None):
+    """Split string and return parts as list if not empty."""
+    return [x for x in string.split(delim) if x]
 
 def run_completion(test_dir, rc_command, expected):
+    """Run a completion test, and compare with EXPECTED output."""
     outputs = run_rc([c.format(test_dir) for c in rc_command]).split("\n")
 
     size = len(outputs)
     assert_that(size, equal_to(len(expected)))
 
-    for i in xrange (0, size):
+    for i in xrange(0, size):
         assert_that(non_empty_split(outputs[i].strip(), " "),
                     equal_to(non_empty_split(expected[i].strip(), " ")))
 
 def run(test_dir, rc_command, expected, test_type):
-    """
-    Run test
-    """
+    """Run test."""
     if test_type == TestType.location:
         run_location(test_dir, rc_command, expected)
     elif test_type == TestType.parse:
@@ -131,9 +129,7 @@ def run(test_dir, rc_command, expected, test_type):
 
 
 def setup_rdm(test_dir, test_files):
-    """
-    Start rdm and parse the test files.
-    """
+    """Start rdm and parse the test files."""
     rdm = sp.Popen([RDM, "-n", SOCKET_FILE, "-d", "~/.rtags_dev", "-o", "-B",
                     "-C", "--log-flush"], stdout=sp.PIPE, stderr=sp.STDOUT)
     compile_commands = create_compile_commands(test_dir, test_files)
@@ -158,15 +154,14 @@ def setup_rdm(test_dir, test_files):
 
 
 def setup_module():
-    """
-    Nosetests module setup function.
-    """
+    """Nosetests module setup function."""
     for exe in [RDM, RC]:
         if not (os.path.isfile(exe) and os.access(exe, os.X_OK)):
             raise FileNotFoundError("{} does not exist or is not executable\n".format(exe))
         return 0
 
 def get_type(test_dir):
+    """Return test type derived from the directory name."""
     if "Parsing" in test_dir:
         return TestType.parse
 
@@ -176,9 +171,7 @@ def get_type(test_dir):
     return TestType.location
 
 def test_generator():
-    """
-    Main nosetests entry point.
-    """
+    """Main nosetests entry point."""
     base_test_dir = os.path.dirname(os.path.abspath(__file__))
     for test_dir, _, test_files in tuple(os.walk(base_test_dir))[1:]:
         if "__pycache__" in test_dir:
