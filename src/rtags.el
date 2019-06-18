b97a635e10c09c0721386a5d85e659cd37a12b9d
Anders Bakken <agbakken@gmail.com>
2019-06-17 22:55:06 -0700

    Bump version to 2.32

--- src/rtags.el
+++ src/rtags.el
@@ -5,7 +5,7 @@
 ;; Author: Jan Erik Hanssen <jhanssen@gmail.com>
 ;;         Anders Bakken <agbakken@gmail.com>
 ;; URL: http://rtags.net
-;; Version: 2.31
+;; Version: 2.32.128
 
 ;; This file is not part of GNU Emacs.
 
@@ -74,7 +74,7 @@
 ;; Constants
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defconst rtags-protocol-version 128)
-(defconst rtags-package-version "2.31")
+(defconst rtags-package-version "2.32")
 (defconst rtags-popup-available (require 'popup nil t))
 (defconst rtags-supported-major-modes '(c-mode c++-mode objc-mode) "Major modes RTags supports.")
 (defconst rtags-verbose-results-delimiter "------------------------------------------")
