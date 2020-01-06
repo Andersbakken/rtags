bc5219d68d22258cc8793af108b76eae55010f0b
Denis Danilov <ddanilov@users.noreply.github.com>
2020-01-06 09:59:32 +0100

    fix missing comma

--- src/Source.cpp
+++ src/Source.cpp
@@ -207,7 +207,7 @@ static const char *blacklist[] = {
     "-fembed-bitcode-marker",
     "-fmodules-validate-once-per-build-session",
     "-fno-delete-null-pointer-checks",
-    "-fno-use-linker-plugin"
+    "-fno-use-linker-plugin",
     "-fno-var-tracking",
     "-fno-var-tracking-assignments",
     "-fno-enforce-eh-specs",
