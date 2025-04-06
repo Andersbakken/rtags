6a023ade2c4b24161f7f1800333dd7ff905d26e2
Anders Bakken <agbakken@gmail.com>
2025-04-06 12:00:45 -0700

    Check for ninja.

--- scripts/rtags-release.sh
+++ scripts/rtags-release.sh
@@ -4,7 +4,11 @@ mkdir build
 cd build
 for generator in TGZ TBZ2; do
     cmake .. -DRTAGS_ENABLE_DEV_OPTIONS=1 $CMAKE_ARGS -DCPACK_GENERATOR=$generator
-    make package_source
+    if [ -e "Makefile" ]; then
+        make package_source
+    else
+        ninja package_source
+    fi
 done
 
 exit 0
