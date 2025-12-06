b0a71e03a5f94571b18eb95c38a8c6216393a902
Anders Bakken <agbakken@gmail.com>
2025-12-06 15:17:17 -0800

    Add -cxx-isystem

--- src/IncludeTypesInternal.h
+++ src/IncludeTypesInternal.h
@@ -13,6 +13,7 @@
    You should have received a copy of the GNU General Public License
    along with RTags.  If not, see <https://www.gnu.org/licenses/>. */
 
+DECLARE_INCLUDE_TYPE(Type_CxxSystem, "-cxx-isystem", " ")
 DECLARE_INCLUDE_TYPE(Type_DirAfter, "-idirafter", " ")
 DECLARE_INCLUDE_TYPE(Type_File, "-include", " ")
 DECLARE_INCLUDE_TYPE(Type_Framework, "-F", "")
